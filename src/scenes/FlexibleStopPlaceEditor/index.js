e56da837cd70ef0c3b10c7e175073ac29da7d702
Sondre Bjerkerud <sondre.bjerkerud@acando.no>
2018-10-05 09:14:21 +0200

    Create a simple flexible stop place form.

--- src/scenes/FlexibleStopPlaceEditor/index.js
+++ src/scenes/FlexibleStopPlaceEditor/index.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Button, Label, TextField } from '@entur/component-library';
+
 import FlexibleStopPlaceMap from './components/FlexibleStopPlaceMap';
 
 import './styles.css';
 
 class FlexibleStopPlaceEditor extends Component {
-  state = { polygon: [] };
+  state = {
+    name: '',
+    validity: '',
+    polygon: []
+  };
+
+  onFieldChange(field, value) {
+    this.setState({ [field]: value });
+  }
 
   handleMapOnClick(e) {
     this.setState(prevState => ({
@@ -13,10 +23,36 @@ class FlexibleStopPlaceEditor extends Component {
   }
 
   render() {
-    const { polygon } = this.state;
+    const { name, validity, polygon } = this.state;
 
     return (
       <div className="flexible-stop-place-editor">
+        <h3>Opprett fleksibelt stoppested</h3>
+
+        <div className="stop-place-form">
+          <div>
+            <Label>Navn</Label>
+            <TextField
+              type="string"
+              value={name}
+              onChange={e => this.onFieldChange('name', e.target.value)}
+            />
+          </div>
+
+          <div>
+            <Label>Validity</Label>
+            <TextField
+              type="string"
+              value={validity}
+              onChange={e => this.onFieldChange('validity', e.target.value)}
+            />
+          </div>
+
+          <div className="save-button-container">
+            <Button variant="success">Lagre</Button>
+          </div>
+        </div>
+
         <FlexibleStopPlaceMap
           onClick={::this.handleMapOnClick}
           polygon={polygon}
