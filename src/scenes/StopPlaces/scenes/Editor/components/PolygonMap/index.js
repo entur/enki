52adbcb18df850458a88fd9996af7f151aa880d3
Sondre Bjerkerud <sondre.bjerkerud@acando.no>
2018-10-26 10:16:06 +0200

    Change the stop place map behaviour so that when creating a new stop place the map will be centered on the users location, while when editing an existing stop place the map will be centered and zoomed (within bounds) according to that polygon.

--- src/scenes/StopPlaces/scenes/Editor/components/PolygonMap/index.js
+++ src/scenes/StopPlaces/scenes/Editor/components/PolygonMap/index.js
@@ -9,27 +9,38 @@ class PolygonMap extends Component {
       lat: 59.91,
       lng: -10.76
     },
+    bounds: undefined,
     zoom: 12
   };
 
   map = createRef();
-  polygon = createRef();
 
-  componentDidMount() {
-    this.map.current.leafletElement.locate({ setView: true });
+  setPolygonRef(element) {
+    if (element && !this.polygonRefLoaded) {
+      const bounds = element.leafletElement.getBounds();
+      if (bounds.isValid()) {
+        this.setState({ bounds });
+      }
+    }
+
+    if(element != null) {
+      this.polygonRefLoaded = true;
+    }
   }
 
-  handleLocationFound = e => {
-    this.setState({ center: e.latlng });
-  };
+  componentDidMount() {
+    this.map.current.leafletElement.locate();
+  }
+
+  handleLocationFound(e) {
+    if (!this.state.bounds) {
+      this.setState({ center: e.latlng });
+    }
+  }
 
   render() {
     const { onClick, polygon } = this.props;
-    const { center, zoom } = this.state;
-
-    const bounds = this.polygon.current
-      ? this.polygon.current.leafletElement.getBounds()
-      : undefined;
+    const { center, bounds, zoom } = this.state;
 
     return (
       <div className="map-container">
@@ -39,14 +50,14 @@ class PolygonMap extends Component {
           zoom={zoom}
           bounds={bounds}
           onClick={onClick}
-          onLocationFound={this.handleLocationFound}
+          onLocationFound={::this.handleLocationFound}
           ref={this.map}
         >
           <TileLayer
             attribution="&amp;copy <a href=&quot;http://osm.org/copyright&quot;>OpenStreetMap</a> contributors"
             url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           />
-          <Polygon ref={this.polygon} positions={polygon} />
+          <Polygon ref={::this.setPolygonRef} positions={polygon} />
         </Map>
       </div>
     );
