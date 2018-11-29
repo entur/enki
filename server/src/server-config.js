89cdf791c2f4506ecbc4bdc8ce38d834acdb20e9
Sondre Bjerkerud <sondre.bjerkerud@acando.no>
2018-11-29 17:28:30 +0100

    Removing the fallback

--- server/src/server-config.js
+++ server/src/server-config.js
@@ -24,7 +24,7 @@ const configureApp = app =>
     .use(compression())
     .use(express.static(contentRoot))
     .use('/' + endpointBase, createRouter())
-    .use(fallback('index.html', { root: contentRoot }))
+    // .use(fallback('index.html', { root: contentRoot }))
     .use((err, req, res, next) => {
       appLog(errorLevel, `Request to ${req.url} failed: ${err.stack}`);
       next(err);
