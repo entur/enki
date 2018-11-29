4c486be6b71a58a2f2ab0cb1cdcc09a9e125ffad
Sondre Bjerkerud <sondre.bjerkerud@acando.no>
2018-11-29 17:09:11 +0100

    Removing the public url from the server.

--- server/src/server-config.js
+++ server/src/server-config.js
@@ -8,12 +8,12 @@ const { appLog, errorLevel } = require('./log');
 const contentRoot = path.resolve(process.env.CONTENT_BASE || '../../build');
 const { withMetrix } = require('./metrix');
 
-const publicUrl = '/admin/flexible-transport';
+// const publicUrl = '/admin/flexible-transport';
 
 const configureApp = app =>
   withSecurity(withMetrix(app))
-    .get(publicUrl + '/health', (req, res) => res.json({ status: 'UP' }))
-    .get(publicUrl + '/info', (req, res) =>
+    .get('/health', (req, res) => res.json({ status: 'UP' }))
+    .get('/info', (req, res) =>
       res.json({
         name: 'order-transport',
         environment: process.env.ENVIRONMENT,
@@ -22,8 +22,8 @@ const configureApp = app =>
       })
     )
     .use(compression())
-    .use(publicUrl + '/static', express.static(contentRoot))
-    .use(publicUrl + '/' + endpointBase, createRouter())
+    .use(express.static(contentRoot))
+    .use('/' + endpointBase, createRouter())
     .use(fallback('index.html', { root: contentRoot }))
     .use((err, req, res, next) => {
       appLog(errorLevel, `Request to ${req.url} failed: ${err.stack}`);
