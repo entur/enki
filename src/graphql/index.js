e3d10a5db3655b0a3a554135c133bc90c6cfd954
Sondre Bjerkerud <sondre.bjerkerud@acando.no>
2019-01-11 13:06:41 +0100

    Attempt at fixing the bug where the GetProviders call sometimes would fail with 401.

--- src/graphql/index.js
+++ src/graphql/index.js
@@ -3,13 +3,12 @@ import { GraphQLClient } from 'graphql-request';
 import { API_BASE } from '../http/http';
 import token from '../http/token';
 
-const headers = {
-  'et-client-name': 'OT',
-  authorization: token.getBearer()
-};
+const staticHeaders = { 'et-client-name': 'OT' };
 
 export const UttuQuery = (provider, query, variables) => {
   const endpoint = API_BASE + '/uttu/' + provider;
-  const client = new GraphQLClient(endpoint, { headers });
+  const client = new GraphQLClient(endpoint, {
+    headers: staticHeaders.concat({ authorization: token.getBearer() })
+  });
   return client.request(query, variables);
 };
