ba76ffd635f0f446462eb90d6c91a14586ab7577
Tom Erik Støwer <testower@gmail.com>
2019-09-13 11:16:21 +0200

    Rewrite networks editor to use hooks to make improvements easier

--- src/actions/networks.js
+++ src/actions/networks.js
@@ -11,86 +11,118 @@ import { getUttuError } from '../helpers/uttu';
 export const REQUEST_NETWORKS = 'REQUEST_NETWORKS';
 export const RECEIVE_NETWORKS = 'RECEIVE_NETWORKS';
 
-const requestNetworks = () => ({
+export const REQUEST_NETWORK = 'REQUEST_NETWORK';
+export const RECEIVE_NETWORK = 'RECEIVE_NETWORK';
+
+export const SAVE_NETWORK = 'SAVE_NETWORK';
+export const SAVED_NETWORK = 'SAVED_NETWORK';
+
+export const DELETE_NETWORK = 'DELETE_NETWORK';
+export const DELETED_NETWORK = 'DELETED_NETWORK';
+
+const requestNetworksActionCreator = () => ({
   type: REQUEST_NETWORKS
 });
 
-const receiveNetworks = networks => ({
+const requestNetworkActionCreator = () => ({
+  type: REQUEST_NETWORK
+});
+
+const receiveNetworksActionCreator = networks => ({
   type: RECEIVE_NETWORKS,
   networks
 });
 
-export const loadNetworks = () => (dispatch, getState) => {
-  dispatch(requestNetworks());
+const receiveNetworkActionCreator = network => ({
+  type: RECEIVE_NETWORK,
+  network
+});
 
-  const activeProvider = getState().providers.active;
-  return UttuQuery(activeProvider, getNetworksQuery, {})
-    .then(data => {
-      const networks = data.networks.map(n => new Network(n));
-      dispatch(receiveNetworks(networks));
-      return Promise.resolve();
-    })
-    .catch(e => {
-      dispatch(
-        showErrorNotification(
-          `En feil oppstod under lastingen av nettverkene: ${getUttuError(e)}`
-        )
-      );
-      return Promise.reject();
-    });
+const saveNetworkActionCreator  = () => ({
+  type: SAVE_NETWORK
+});
+
+const savedNetworkActionCreator = () => ({
+  type: SAVED_NETWORK
+});
+
+const deleteNetworkActionCreator = () => ({
+  type: DELETE_NETWORK
+});
+
+const deletedNetworkActionCreator = (id) => ({
+  type: DELETED_NETWORK,
+  id
+});
+
+export const loadNetworks = () => async (dispatch, getState) => {
+  dispatch(requestNetworksActionCreator());
+
+  try {
+    const data = await UttuQuery(getState().providers.active, getNetworksQuery, {});
+    const networks = data.networks.map(n => new Network(n));
+    dispatch(receiveNetworksActionCreator(networks));
+  } catch (e) {
+    dispatch(
+      showErrorNotification(
+        `En feil oppstod under lastingen av nettverkene: ${getUttuError(e)}`
+      )
+    );
+  }
 };
 
-export const loadNetworkById = id => (dispatch, getState) => {
-  const activeProvider = getState().providers.active;
-  return UttuQuery(activeProvider, getNetworkByIdQuery, { id })
-    .then(data => Promise.resolve(new Network(data.network)))
-    .catch(e => {
-      dispatch(
-        showErrorNotification(
-          'Laste nettverk',
-          `En feil oppstod under lastingen av nettverket: ${getUttuError(e)}`
-        )
-      );
-      return Promise.reject();
-    });
+export const loadNetworkById = id => async (dispatch, getState) => {
+  dispatch(requestNetworkActionCreator());
+
+  try {
+    const data = await UttuQuery(getState().providers.active, getNetworkByIdQuery, { id });
+    dispatch(receiveNetworkActionCreator(new Network(data.network)));
+  } catch (e) {
+    dispatch(
+      showErrorNotification(
+        'Laste nettverk',
+        `En feil oppstod under lastingen av nettverket: ${getUttuError(e)}`
+      )
+    );
+  }
 };
 
-export const saveNetwork = network => (dispatch, getState) => {
-  const activeProvider = getState().providers.active;
-  return UttuQuery(activeProvider, networkMutation, { input: network })
-    .then(() => {
-      dispatch(
-        showSuccessNotification('Lagre nettverk', 'Nettverket ble lagret.')
-      );
-      return Promise.resolve();
-    })
-    .catch(e => {
-      dispatch(
-        showErrorNotification(
-          'Lagre nettverk',
-          `En feil oppstod under lagringen av nettverket: ${getUttuError(e)}`
-        )
-      );
-      return Promise.reject();
-    });
+export const saveNetwork = network => async (dispatch, getState) => {
+  dispatch(saveNetworkActionCreator());
+
+  try {
+    await UttuQuery(getState().providers.active, networkMutation, { input: network });
+    dispatch(savedNetworkActionCreator());
+    dispatch(
+      showSuccessNotification('Lagre nettverk', 'Nettverket ble lagret.')
+    );
+  } catch (e) {
+    dispatch(savedNetworkActionCreator(null));
+    dispatch(
+      showErrorNotification(
+        'Lagre nettverk',
+        `En feil oppstod under lagringen av nettverket: ${getUttuError(e)}`
+      )
+    );
+  }
 };
 
-export const deleteNetworkById = id => (dispatch, getState) => {
-  const activeProvider = getState().providers.active;
-  return UttuQuery(activeProvider, deleteNetwork, { id })
-    .then(() => {
-      dispatch(
-        showSuccessNotification('Slette nettverk', 'Nettverket ble slettet.')
-      );
-      return Promise.resolve();
-    })
-    .catch(e => {
-      dispatch(
-        showErrorNotification(
-          'Slette nettverk',
-          `En feil oppstod under slettingen av nettverket: ${getUttuError(e)}`
-        )
-      );
-      return Promise.reject();
-    });
+export const deleteNetworkById = id => async (dispatch, getState) => {
+  dispatch(deleteNetworkActionCreator());
+
+  try {
+    await UttuQuery(getState().providers.active, deleteNetwork, { id });
+    dispatch(deletedNetworkActionCreator(id));
+    dispatch(
+      showSuccessNotification('Slette nettverk', 'Nettverket ble slettet.')
+    );
+  } catch (e) {
+    dispatch(deletedNetworkActionCreator());
+    dispatch(
+      showErrorNotification(
+        'Slette nettverk',
+        `En feil oppstod under slettingen av nettverket: ${getUttuError(e)}`
+      )
+    );
+  }
 };
