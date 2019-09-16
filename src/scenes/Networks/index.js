3a9deb9ccd0c41dcd182f219e51c02cc9024b002
Tom Erik Støwer <testower@gmail.com>
2019-09-16 12:22:32 +0200

    Reload networks when active provider changes

--- src/scenes/Networks/index.js
+++ src/scenes/Networks/index.js
@@ -1,6 +1,5 @@
-import React, { Component } from 'react';
-import { compose } from 'redux';
-import { connect } from 'react-redux';
+import React, { useEffect, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
 import { AddIcon } from '@entur/component-library';
 
@@ -16,78 +15,76 @@ import { loadNetworks } from '../../actions/networks';
 
 import './styles.css';
 
-class Networks extends Component {
-  componentDidMount() {
-    this.props.dispatch(loadNetworks());
-  }
+const Networks = ({ history }) => {
+  const { activeProvider, organisations, networks } = useSelector(({ providers, organisations, networks }) => ({
+    activeProvider: providers.active,
+    organisations,
+    networks
+  }));
 
-  handleOnRowClick(id) {
-    const { history } = this.props;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadNetworks());
+  }, [dispatch, loadNetworks, activeProvider]);
+
+  const handleOnRowClick = useCallback((id) => {
     history.push(`/networks/edit/${id}`);
-  }
+  }, [history]);
 
-  render() {
-    const { organisations, networks } = this.props;
-
-    const renderTableRows = () => {
-      if (networks) {
-        return networks.length > 0 ? (
-          networks.map(n => (
-            <TableRow
-              key={n.id}
-              onClick={() => this.handleOnRowClick(n.id)}
-              title={n.description}
-            >
-              <TableRowCell>{n.name}</TableRowCell>
-              <TableRowCell>{n.privateCode}</TableRowCell>
-              <TableRowCell>
-                {organisations.find(o => o.id === n.authorityRef).name}
-              </TableRowCell>
-            </TableRow>
-          ))
-        ) : (
-          <TableRow className="row-no-networks disabled">
-            <TableRowCell colSpan={3}>Ingen nettverk ble funnet.</TableRowCell>
-          </TableRow>
-        );
-      } else {
-        return (
-          <TableRow className="disabled">
-            <TableRowCell colSpan={3}>
-              <Loading text="Laster inn nettverk..." />
+  const renderTableRows = () => {
+    if (networks) {
+      return networks.length > 0 ? (
+        networks.map(n => (
+          <TableRow
+            key={n.id}
+            onClick={() => handleOnRowClick(n.id)}
+            title={n.description}
+          >
+            <TableRowCell>{n.name}</TableRowCell>
+            <TableRowCell>{n.privateCode}</TableRowCell>
+            <TableRowCell>
+              {organisations.find(o => o.id === n.authorityRef).name}
             </TableRowCell>
           </TableRow>
-        );
-      }
-    };
+        ))
+      ) : (
+        <TableRow className="row-no-networks disabled">
+          <TableRowCell colSpan={3}>Ingen nettverk ble funnet.</TableRowCell>
+        </TableRow>
+      );
+    } else {
+      return (
+        <TableRow className="disabled">
+          <TableRowCell colSpan={3}>
+            <Loading text="Laster inn nettverk..." />
+          </TableRowCell>
+        </TableRow>
+      );
+    }
+  };
 
-    return (
-      <div className="networks">
-        <h2>Nettverk</h2>
+  return (
+    <div className="networks">
+      <h2>Nettverk</h2>
 
-        <Link to="/networks/create">
-          <IconButton
-            icon={<AddIcon />}
-            label="Opprett nettverk"
-            labelPosition="right"
-          />
-        </Link>
+      <Link to="/networks/create">
+        <IconButton
+          icon={<AddIcon />}
+          label="Opprett nettverk"
+          labelPosition="right"
+        />
+      </Link>
 
-        <Table>
-          <TableHeaderCell label="Navn" />
-          <TableHeaderCell label="Privat kode" />
-          <TableHeaderCell label="Autoritet" />
+      <Table>
+        <TableHeaderCell label="Navn" />
+        <TableHeaderCell label="Privat kode" />
+        <TableHeaderCell label="Autoritet" />
 
-          {renderTableRows()}
-        </Table>
-      </div>
-    );
-  }
+        {renderTableRows()}
+      </Table>
+    </div>
+  );
 }
 
-const mapStateToProps = ({ organisations, networks }) => ({
-  organisations,
-  networks
-});
-
-export default compose(withRouter, connect(mapStateToProps))(Networks);
+export default withRouter(Networks);
