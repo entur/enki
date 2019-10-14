eaf193ff1980575b5e46f3f781a6a55f6c701ade
Tom Erik Støwer <testower@gmail.com>
2019-10-14 12:13:07 +0200

    ROR-680 Add uttu error for from date > to date

--- src/helpers/uttu.messages.js
+++ src/helpers/uttu.messages.js
@@ -8,5 +8,9 @@ export default defineMessages({
   ORGANISATION_NOT_VALID_OPERATOR: {
     id: 'helpers.uttu.uttuError.ORGANISATION_NOT_VALID_OPERATOR',
     defaultMessage: 'Ugyldig operatør'
+  },
+  FROM_DATE_AFTER_TO_DATE: {
+    id: 'helpers.uttu.uttuError.FROM_DATE_AFTER_TO_DATE',
+    defaultMessage: 'Fra dato kan ikke være etter til dato'
   }
 });
