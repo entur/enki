6a91c98971d31a7a047bb7fe3f7567dbd2842227
Tom Erik Støwer <testower@gmail.com>
2019-10-16 06:19:55 +0200

    ROR-680 i18n for stop places table

---
+++ src/scenes/StopPlaces/messages.js
@@ -0,0 +1,32 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'stopPlaces.header',
+    defaultMessage: 'Stoppesteder'
+  },
+  createStopPlaceLinkIconLabelText: {
+    id: 'stopPlaces.createStopPlaceLinkIconLabelText',
+    defaultMessage: 'Opprett stoppested'
+  },
+  nameTableHeaderLabelText: {
+    id: 'stopPlaces.nameTableHeaderLabelText',
+    defaultMessage: 'Navn'
+  },
+  privateCodeTableHeaderLabelText: {
+    id: 'stopPlaces.privateCodeTableHeaderLabelText',
+    defaultMessage: 'Privat kode'
+  },
+  numberOfPointsTableHeaderLabelText: {
+    id: 'stopPlaces.numberOfPointsTableHeaderLabelText',
+    defaultMessage: 'Antall punkter'
+  },
+  noStopPlacesFoundText: {
+    id: 'stopPlaces.noStopPlacesFoundText',
+    defaultMessage: 'Ingen stoppesteder ble funnet.'
+  },
+  loadingStopPlacesText: {
+    id: 'stopPlaces.loadingStopPlacesText',
+    defaultMessage: 'Laster inn stoppesteder...'
+  }
+});
