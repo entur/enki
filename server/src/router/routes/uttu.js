43f40ed5e7545ddfa2c0d6ec6ddd9006e02f0646
Tom Erik Støwer <testower@gmail.com>
2019-10-29 15:31:21 +0100

    Fix: Missing route for export download in forwarding proxy

--- server/src/router/routes/uttu.js
+++ server/src/router/routes/uttu.js
@@ -9,6 +9,11 @@ const proxy = forwardingProxy(apiUrl);
 console.log(`UTTU proxy forwarding to ${apiUrl}`);
 
 router.use(
+  '/uttu/:provider/export/:id/download',
+  proxy([OPTION, POST], req => `/${req.params.provider}/export/${req.params.id}/download`)
+);
+
+router.use(
   '/uttu/:provider',
   proxy([OPTION, POST], req => `/${req.params.provider}/graphql`)
 );
