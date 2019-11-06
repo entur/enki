d7e14baed2ffbd8a3ed3da345710de3f07162ae3
Tom Erik Støwer <testower@gmail.com>
2019-11-06 11:50:21 +0100

    ROR-713 Complete refactoring

--- src/scenes/Lines/scenes/Editor/BookingArrangementEditor/index.js
+++ src/scenes/Lines/scenes/Editor/BookingArrangementEditor/index.js
@@ -1,9 +1,6 @@
-import React, { Component } from 'react';
+import React, { useCallback } from 'react';
 import PropTypes from 'prop-types';
-
 import { BookingArrangement, Contact } from '../../../../../model';
-
-import './styles.css';
 import ContactFields from './contactFields';
 import BookingLimitFields from './bookingLimitFields';
 import BookingMethodSelection from './bookingMethodSelection';
@@ -11,11 +8,15 @@ import BookingTimeSelection from './bookingTimeSelection';
 import PaymentTimeSelection from './paymentTimeSelection';
 import BookingAccessSelection from './bookingAccessSelection';
 import BookingNoteField from './bookingNoteField';
+import './styles.css';
 
-class BookingArrangementEditor extends Component {
-  handleFieldChange(field, value, multi = false) {
-    const { bookingArrangement, onChange } = this.props;
+const BookingArrangementEditor = props => {
+  const {
+    bookingArrangement = {},
+    onChange
+  } = props;
 
+  const handleFieldChange = useCallback((field, value, multi = false) => {
     let newValue = value;
     if (multi) {
       if (bookingArrangement) {
@@ -32,10 +33,9 @@ class BookingArrangementEditor extends Component {
       : new BookingArrangement({ [field]: newValue });
 
     onChange(newBooking.isEmpty() ? undefined : newBooking);
-  }
+  }, [bookingArrangement, onChange]);
 
-  handleContactFieldChange(field, value) {
-    const { bookingArrangement } = this.props;
+  const handleContactFieldChange = useCallback((field, value) => {
     let newContact;
     if (bookingArrangement && bookingArrangement.bookingContact) {
       newContact = bookingArrangement.bookingContact.withChanges({
@@ -44,14 +44,13 @@ class BookingArrangementEditor extends Component {
     } else {
       newContact = new Contact({ [field]: value });
     }
-    this.handleFieldChange(
+    handleFieldChange(
       'bookingContact',
       newContact.isEmpty() ? undefined : newContact
     );
-  }
+  }, [bookingArrangement, handleFieldChange]);
 
-  resetBookingLimit() {
-    const { bookingArrangement, onChange } = this.props;
+  const resetBookingLimit = useCallback(() => {
     if (bookingArrangement) {
       onChange(
         bookingArrangement.withChanges({
@@ -60,67 +59,63 @@ class BookingArrangementEditor extends Component {
         })
       );
     }
-  }
+  }, [bookingArrangement, onChange]);
 
-  render() {
-    const {
-      bookingArrangement: {
-        bookingContact,
-        bookingMethods,
-        bookWhen,
-        latestBookingTime = null,
-        minimumBookingPeriod = null,
-        buyWhen,
-        bookingAccess,
-        bookingNote
-      } = {}
-    } = this.props;
+  const {
+    bookingContact,
+    bookingMethods,
+    bookWhen,
+    latestBookingTime = null,
+    minimumBookingPeriod = null,
+    buyWhen,
+    bookingAccess,
+    bookingNote
+  } = bookingArrangement;
 
-    return (
-      <div className="booking-editor">
-        <ContactFields
-          contact={bookingContact}
-          handleContactFieldChange={this.handleContactFieldChange.bind(this)}
-        />
+  return (
+    <div className="booking-editor">
+      <ContactFields
+        contact={bookingContact}
+        handleContactFieldChange={handleContactFieldChange}
+      />
 
-        <BookingMethodSelection
-          bookingMethods={bookingMethods}
-          handleFieldChange={this.handleFieldChange.bind(this)}
-        />
+      <BookingMethodSelection
+        bookingMethods={bookingMethods}
+        handleFieldChange={handleFieldChange}
+      />
 
-        <BookingTimeSelection
-          bookWhen={bookWhen}
-          handleFieldChange={this.handleFieldChange.bind(this)}
-        />
+      <BookingTimeSelection
+        bookWhen={bookWhen}
+        handleFieldChange={handleFieldChange}
+      />
 
-        <BookingLimitFields
-          latestBookingTime={latestBookingTime}
-          minimumBookingPeriod={minimumBookingPeriod}
-          resetBookingLimit={this.resetBookingLimit.bind(this)}
-          handleFieldChange={this.handleFieldChange.bind(this)}
-        />
+      <BookingLimitFields
+        latestBookingTime={latestBookingTime}
+        minimumBookingPeriod={minimumBookingPeriod}
+        resetBookingLimit={resetBookingLimit}
+        handleFieldChange={handleFieldChange}
+      />
 
-        <PaymentTimeSelection
-          buyWhen={buyWhen}
-          handleFieldChange={this.handleFieldChange.bind(this)}
-        />
+      <PaymentTimeSelection
+        buyWhen={buyWhen}
+        handleFieldChange={handleFieldChange}
+      />
 
-        <BookingAccessSelection
-          bookingAccess={bookingAccess}
-          handleFieldChange={this.handleFieldChange.bind(this)}
-        />
+      <BookingAccessSelection
+        bookingAccess={bookingAccess}
+        handleFieldChange={handleFieldChange}
+      />
 
-        <BookingNoteField
-          bookingNote={bookingNote}
-          handleFieldChange={this.handleFieldChange.bind(this)}
-        />
-
-      </div>
-    );
-  }
+      <BookingNoteField
+        bookingNote={bookingNote}
+        handleFieldChange={handleFieldChange}
+      />
+    </div>
+  );
 }
 
 BookingArrangementEditor.propTypes = {
+  onChange: PropTypes.func.isRequired,
   bookingArrangement: PropTypes.instanceOf(BookingArrangement)
 };
 
