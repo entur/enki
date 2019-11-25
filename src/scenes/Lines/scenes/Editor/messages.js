e1a5b19baf84f83fa36bc4353dfac1ffd71e0bbb
Tom Erik Støwer <testower@gmail.com>
2019-11-25 14:16:53 +0100

    ROR-680 i18n for line editor

---
+++ src/scenes/Lines/scenes/Editor/messages.js
@@ -0,0 +1,64 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  createLineHeader: {
+    id: 'lines.editor.createLineHeader',
+    defaultMessage: 'Opprett linje'
+  },
+  editLineHeader: {
+    id: 'lines.editor.editLineHeader',
+    defaultMessage: 'Rediger linje'
+  },
+  saveButtonText: {
+    id: 'lines.editor.saveButtonText',
+    defaultMessage: 'Lagre'
+  },
+  deleteButtonText: {
+    id: 'lines.editor.deleteButtonText',
+    defaultMessage: 'Slett'
+  },
+  saveLineLoadingText: {
+    id: 'lines.editor.saveLineLoadingText',
+    defaultMessage: 'Lagrer linjen...'
+  },
+  deleteLineLoadingText: {
+    id: 'lines.editor.deleteLineLoadingText',
+    defaultMessage: 'Sletter linjen...'
+  },
+  generalTabLabel: {
+    id: 'lines.editor.generalTabLabel',
+    defaultMessage: 'Generelt'
+  },
+  journeyPatternsTabLabel: {
+    id: 'lines.editor.journeyPatternsTabLabel',
+    defaultMessage: 'Journey patterns'
+  },
+  bookingTabLabel: {
+    id: 'lines.editor.bookingTabLabel',
+    defaultMessage: 'Bestilling'
+  },
+  loadingLineText: {
+    id: 'lines.editor.loadingLineText',
+    defaultMessage: 'Laster inn linje'
+  },
+  loadingNetworkAndStopsText: {
+    id: 'lines.editor.loadingNetworkAndStopsText',
+    defaultMessage: 'Laster inn nettverk og stoppesteder'
+  },
+  deleteConfirmationDialogTitle: {
+    id: 'lines.editor.deleteConfirmationDialogTitle',
+    defaultMessage: 'Slette linje'
+  },
+  deleteConfirmationDialogMessage: {
+    id: 'lines.editor.deleteConfirmationDialogMessage',
+    defaultMessage: 'Er du sikker på at du ønsker å slette denne linjen?'
+  },
+  deleteConfirmationDialogCancelButtonText: {
+    id: 'lines.editor.deleteConfirmationDialogCancelButtonText',
+    defaultMessage: 'Nei'
+  },
+  deleteConfirmationDialogConfirmButtonText: {
+    id: 'lines.editor.deleteConfirmationDialogConfirmButtonText',
+    defaultMessage: 'Ja'
+  }
+});
