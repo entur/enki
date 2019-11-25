d2c4206c3485b78a507e320db50ffde77694d18f
Tom Erik Støwer <testower@gmail.com>
2019-11-25 13:21:39 +0100

    ROR-680 Refactor and i18n for lines root scene

---
+++ src/scenes/Lines/messages.js
@@ -0,0 +1,32 @@
+import { defineMessages } from 'react-intl';
+
+export default defineMessages({
+  header: {
+    id: 'lines.header',
+    defaultMessage: 'Linjer'
+  },
+  createLineIconButtonLabel: {
+    id: 'lines.createLineIconButtonLabel',
+    defaultMessage: 'Opprett linje'
+  },
+  nameTableHeaderLabel: {
+    id: 'lines.nameTableHeaderLabel',
+    defaultMessage: 'Navn'
+  },
+  privateCodeTableHeaderLabel: {
+    id: 'lines.privateCodeTableHeaderLabel',
+    defaultMessage: 'Privat kode'
+  },
+  operatorTableHeader: {
+    id: 'lines.operatorTableHeader',
+    defaultMessage: 'Operatør'
+  },
+  noLinesFoundText: {
+    id: 'lines.noLinesFoundText',
+    defaultMessage: 'Ingen linjer ble funnet.'
+  },
+  loadingText: {
+    id: 'lines.loadingText',
+    defaultMessage: 'Laster inn linjer...'
+  }
+});
