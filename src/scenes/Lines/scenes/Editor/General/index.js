fa649f0df44b413d7f2abcef900552be2e1d4c08
Fredrik Meyer <hrmeyer@gmail.com>
2020-01-17 13:15:11 +0100

    Rediger linje->designsystem + litt enkel validering

--- src/scenes/Lines/scenes/Editor/General/index.js
+++ src/scenes/Lines/scenes/Editor/General/index.js
@@ -1,17 +1,16 @@
 import React, { Fragment } from 'react';
 import { useSelector } from 'react-redux';
-import {
-  TextField,
-  TextArea,
-  DropDown,
-  DropDownOptions,
-  FormGroup
-} from '@entur/component-library';
-import { DEFAULT_SELECT_VALUE, DEFAULT_SELECT_LABEL } from '../constants';
+import { Dropdown } from '@entur/dropdown';
+import { TextArea, TextField, InputGroup } from '@entur/form';
+import { DEFAULT_SELECT_LABEL, DEFAULT_SELECT_VALUE } from '../constants';
 import Errors from 'components/Errors';
 import { FLEXIBLE_LINE_TYPE } from 'model/enums';
 import { selectIntl } from 'i18n';
 import messages from './messages';
+
+function inputStringIsEmpty(input) {
+  return !input || input.trim() === '';
+}
 
 export default ({
   flexibleLine,
@@ -27,112 +26,98 @@ export default ({
   const { formatMessage } = useSelector(selectIntl);
   return (
     <Fragment>
-      <FormGroup
+      <InputGroup
         className="form-section"
-        inputId="name"
-        title={formatMessage(messages.nameFormGroupTitle)}
+        label={formatMessage(messages.nameFormGroupTitle)}
+        variant={inputStringIsEmpty(flexibleLine.name) ? 'error' : undefined}
+        feedback={
+          inputStringIsEmpty(flexibleLine.name)
+            ? 'Navn må fylles inn.'
+            : undefined
+        }
       >
         <TextField
-          type="text"
-          value={flexibleLine.name}
+          defaultValue={flexibleLine.name}
           onChange={e => handleFieldChange('name', e.target.value)}
         />
-      </FormGroup>
+      </InputGroup>
 
-      <FormGroup
+      <InputGroup
         className="form-section"
-        inputId="description"
-        title={formatMessage(messages.descriptionFormGroupTitle)}
+        label={formatMessage(messages.descriptionFormGroupTitle)}
       >
         <TextArea
           type="text"
-          value={flexibleLine.description}
+          value={flexibleLine.description || ''}
           onChange={e => handleFieldChange('description', e.target.value)}
         />
-      </FormGroup>
+      </InputGroup>
 
-      <FormGroup
+      <InputGroup
         className="form-section"
-        inputId="privateCode"
-        title={formatMessage(messages.privateCodeFormGroupTitle)}
+        label={formatMessage(messages.privateCodeFormGroupTitle)}
       >
         <TextField
           type="text"
-          value={flexibleLine.privateCode}
+          value={flexibleLine.privateCode || ''}
           onChange={e => handleFieldChange('privateCode', e.target.value)}
         />
-      </FormGroup>
+      </InputGroup>
 
-      <FormGroup
+      <InputGroup
         className="form-section"
-        inputId="publicCode"
-        title={formatMessage(messages.publicCodeFormGroupTitle)}
+        label={formatMessage(messages.publicCodeFormGroupTitle)}
+        variant={
+          inputStringIsEmpty(flexibleLine.publicCode) ? 'error' : undefined
+        }
+        feedback={
+          inputStringIsEmpty(flexibleLine.publicCode)
+            ? 'Public Code må fylles inn.'
+            : undefined
+        }
       >
         <TextField
           type="text"
-          value={flexibleLine.publicCode}
+          defaultValue={flexibleLine.publicCode}
           onChange={e => handleFieldChange('publicCode', e.target.value)}
         />
-      </FormGroup>
+      </InputGroup>
 
-      <FormGroup
+      <Dropdown
         className="form-section"
-        inputId="operator"
-        title={formatMessage(messages.operatorFormGroupTitle)}
-      >
-        <DropDown
-          value={operatorSelection}
-          onChange={e => handleOperatorSelectionChange(e.target.value)}
-        >
-          <DropDownOptions
-            label={DEFAULT_SELECT_LABEL}
-            value={DEFAULT_SELECT_VALUE}
-          />
-          {operators.map(o => (
-            <DropDownOptions key={o.name} label={o.name} value={o.id} />
-          ))}
-        </DropDown>
-      </FormGroup>
+        searchable
+        items={operators.map(o => ({ value: o.id, label: o.name }))}
+        label={formatMessage(messages.operatorFormGroupTitle)}
+        onChange={({ value }) => handleOperatorSelectionChange(value)}
+        placeholder="F.eks Østfold Kollektivtrafikk"
+        value={operatorSelection}
+      />
 
-      <FormGroup
+      <Dropdown
         className="form-section"
-        inputId="network"
-        title={formatMessage(messages.networkFormGroupTitle)}
-      >
-        <DropDown
-          value={networkSelection}
-          onChange={e => handleNetworkSelectionChange(e.target.value)}
-          className={errors.networkRef.length > 0 ? 'input-error' : ''}
-        >
-          <DropDownOptions
-            label={DEFAULT_SELECT_LABEL}
-            value={DEFAULT_SELECT_VALUE}
-          />
-          {networks.map(n => (
-            <DropDownOptions key={n.name} label={n.name} value={n.id} />
-          ))}
-        </DropDown>
-      </FormGroup>
+        items={networks.map(n => ({ value: n.id, label: n.name }))}
+        label={formatMessage(messages.networkFormGroupTitle)}
+        onChange={({ value }) => handleNetworkSelectionChange(value)}
+        placeHolder={DEFAULT_SELECT_LABEL}
+        value={networkSelection}
+        variant={errors.networkRef.length > 0 ? 'input-error' : undefined}
+      />
+
       <Errors errors={errors.networkRef} />
 
-      <FormGroup
+      <Dropdown
         className="form-section"
-        inputId="flexibleLineType"
-        title={formatMessage(messages.typeFormGroupTitle)}
-      >
-        <DropDown
-          value={flexibleLine.flexibleLineType}
-          onChange={e => handleFieldChange('flexibleLineType', e.target.value)}
-        >
-          <DropDownOptions
-            label={DEFAULT_SELECT_LABEL}
-            value={DEFAULT_SELECT_VALUE}
-          />
-          {Object.values(FLEXIBLE_LINE_TYPE).map(type => (
-            <DropDownOptions key={type} label={type} value={type} />
-          ))}
-        </DropDown>
-      </FormGroup>
+        items={[
+          { value: DEFAULT_SELECT_VALUE, label: DEFAULT_SELECT_LABEL },
+          ...Object.values(FLEXIBLE_LINE_TYPE).map(type => ({
+            value: type,
+            label: type
+          }))
+        ]}
+        label={formatMessage(messages.typeFormGroupTitle)}
+        onChange={e => handleFieldChange('flexibleLineType', e.value)}
+        value={flexibleLine.flexibleLineType}
+      />
     </Fragment>
   );
 };
