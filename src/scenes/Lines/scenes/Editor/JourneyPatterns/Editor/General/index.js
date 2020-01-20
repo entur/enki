ccba494e0244d8ce2e11e9b78c82cb229a2f4a5d
Fredrik Meyer <hrmeyer@gmail.com>
2020-01-20 13:34:24 +0100

    Bytt til designsystem i nok en av visningene i editoren
    Pluss noe veldig enkel feilhåndtering.

--- src/scenes/Lines/scenes/Editor/JourneyPatterns/Editor/General/index.js
+++ src/scenes/Lines/scenes/Editor/JourneyPatterns/Editor/General/index.js
@@ -1,16 +1,12 @@
 import React, { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
-import {
-  Label,
-  TextField,
-  TextArea,
-  DropDown,
-  DropDownOptions
-} from '@entur/component-library';
+import { Dropdown } from '@entur/dropdown';
+import { InputGroup, TextArea, TextField } from '@entur/form';
 
 import { JourneyPattern } from 'model';
 import { DIRECTION_TYPE } from 'model/enums';
+import { isBlank } from 'helpers/forms';
 import { DEFAULT_SELECT_VALUE, DEFAULT_SELECT_LABEL } from '../../../constants';
 import messages from './messages';
 import { selectIntl } from 'i18n';
@@ -22,43 +18,54 @@ const General = ({
   handleDirectionSelectionChange
 }) => {
   const { formatMessage } = useSelector(selectIntl);
+  console.log(journeyPattern.name);
 
   return (
     <Fragment>
-      <Label>{formatMessage(messages.nameLabel)}</Label>
-      <TextField
-        type="text"
-        value={journeyPattern.name}
-        onChange={e => onFieldChange('name', e.target.value)}
-      />
-
-      <Label>{formatMessage(messages.descriptionLabel)}</Label>
-      <TextArea
-        type="text"
-        value={journeyPattern.description}
-        onChange={e => onFieldChange('description', e.target.value)}
-      />
-
-      <Label>{formatMessage(messages.privateCodeLabel)}</Label>
-      <TextField
-        type="text"
-        value={journeyPattern.privateCode}
-        onChange={e => onFieldChange('privateCode', e.target.value)}
-      />
-
-      <Label>{formatMessage(messages.directionLabel)}</Label>
-      <DropDown
-        value={directionSelection}
-        onChange={e => handleDirectionSelectionChange(e.target.value)}
+      <InputGroup
+        className="form-section"
+        label={formatMessage(messages.nameLabel)}
+        feedback={
+          isBlank(journeyPattern.name) ? 'Navn må fylles inn.' : undefined
+        }
+        variant={isBlank(journeyPattern.name) ? 'error' : undefined}
       >
-        <DropDownOptions
-          label={DEFAULT_SELECT_LABEL}
-          value={DEFAULT_SELECT_VALUE}
+        <TextField
+          defaultValue={journeyPattern.name}
+          onChange={e => onFieldChange('name', e.target.value)}
         />
-        {Object.values(DIRECTION_TYPE).map(dt => (
-          <DropDownOptions key={dt} label={dt} value={dt} />
-        ))}
-      </DropDown>
+      </InputGroup>
+
+      <InputGroup
+        label={formatMessage(messages.descriptionLabel)}
+        className="form-section"
+      >
+        <TextArea
+          value={journeyPattern.description || ''}
+          onChange={e => onFieldChange('description', e.target.value)}
+        />
+      </InputGroup>
+
+      <InputGroup
+        className="form-section"
+        label={formatMessage(messages.privateCodeLabel)}
+      >
+        <TextField
+          value={journeyPattern.privateCode || ''}
+          onChange={e => onFieldChange('privateCode', e.target.value)}
+        />
+      </InputGroup>
+
+      <InputGroup label={formatMessage(messages.directionLabel)}>
+        <Dropdown
+          items={[
+            { value: DEFAULT_SELECT_VALUE, label: DEFAULT_SELECT_LABEL },
+            ...Object.values(DIRECTION_TYPE)
+          ]}
+          value={DEFAULT_SELECT_VALUE}
+          onChange={({ value }) => handleDirectionSelectionChange(value)}
+        />
+      </InputGroup>
     </Fragment>
   );
 };
