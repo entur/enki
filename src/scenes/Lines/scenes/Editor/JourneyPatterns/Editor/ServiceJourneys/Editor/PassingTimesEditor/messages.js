1c857916dde4a73ee24eadcaa07eefc0ba26ed76
Aryan Iranzamini <arre_o21@hotmail.com>
2020-02-06 15:23:33 +0100

    add period at end of sentence

--- src/scenes/Lines/scenes/Editor/JourneyPatterns/Editor/ServiceJourneys/Editor/PassingTimesEditor/messages.js
+++ src/scenes/Lines/scenes/Editor/JourneyPatterns/Editor/ServiceJourneys/Editor/PassingTimesEditor/messages.js
@@ -3,23 +3,23 @@ import { defineMessages } from 'react-intl';
 export default defineMessages({
   atleastOneField: {
     id: 'passingTimes.error.atleastOneField',
-    defaultMessage: 'Minst ett felt må være fylt ut'
+    defaultMessage: 'Minst ett felt må være fylt ut.'
   },
   departureAfterArrival: {
     id: 'passingTimes.error.departureAfterArrival',
-    defaultMessage: 'Avgangstid kan ikke være før ankomsttid'
+    defaultMessage: 'Avgangstid kan ikke være før ankomsttid.'
   },
   departureAfterEarliest: {
     id: 'passingTimes.error.departureAfterEarliest',
-    defaultMessage: 'Avgangstid kan ikke være før tidligst avgangstid'
+    defaultMessage: 'Avgangstid kan ikke være før tidligst avgangstid.'
   },
   arrivalBeforeLatest: {
     id: 'passingTimes.error.arrivalBeforeLatest',
-    defaultMessage: 'Ankomsttid ikke kan være senere enn senest ankomsttid'
+    defaultMessage: 'Ankomsttid ikke kan være senere enn senest ankomsttid.'
   },
   laterThanPrevious: {
     id: 'passingTimes.error.laterThanPrevious',
     defaultMessage:
-      'Tidspunktene må være senere enn forrige stoppesteds tidspunkt'
+      'Tidspunktene må være senere enn forrige stoppesteds tidspunkt.'
   }
 });
