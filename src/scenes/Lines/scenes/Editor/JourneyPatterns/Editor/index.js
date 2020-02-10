691c3e3bdb632b8275b0c32a612982d80288e47b
Aryan Iranzamini <arre_o21@hotmail.com>
2020-02-10 14:22:15 +0100

    translations for journey patterns & service journeys

--- src/scenes/Lines/scenes/Editor/JourneyPatterns/Editor/index.js
+++ src/scenes/Lines/scenes/Editor/JourneyPatterns/Editor/index.js
@@ -1,4 +1,6 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { selectIntl } from 'i18n';
 import PropTypes from 'prop-types';
 import { SuccessButton } from '@entur/button';
 import { Tabs, Tab, TabList, TabPanels, TabPanel } from '@entur/tab';
@@ -6,97 +8,99 @@ import PageHeader from 'components/PageHeader';
 import { JourneyPattern } from 'model';
 import StopPointsEditor from './StopPoints';
 import ServiceJourneysEditor from './ServiceJourneys';
+import messages from './messages';
 
 import './styles.scss';
 import General from './General';
 
 import { DEFAULT_SELECT_VALUE } from '../../constants';
 
-class JourneyPatternEditor extends Component {
-  state = {
-    stopPlaceSelection: DEFAULT_SELECT_VALUE,
-    directionSelection: DEFAULT_SELECT_VALUE
+const JourneyPatternEditor = ({
+  journeyPattern,
+  isEditMode,
+  onSave,
+  onChange,
+  onClose
+}) => {
+  const [directionSelection, setDirectionSelection] = useState(
+    DEFAULT_SELECT_VALUE
+  );
+  const { pointsInSequence, directionType, serviceJourneys } = journeyPattern;
+  const { formatMessage } = useSelector(selectIntl);
+
+  useEffect(() => {
+    setDirectionSelection(directionType || DEFAULT_SELECT_VALUE);
+  }, [directionType]);
+
+  const onFieldChange = (field, value) => {
+    onChange(journeyPattern.withFieldChange(field, value));
   };
 
-  componentDidMount() {
-    const { journeyPattern } = this.props;
-    this.setState({
-      stopPlaceSelection:
-        journeyPattern.pointsInSequence.length > 0
-          ? journeyPattern.pointsInSequence[0].flexibleStopPlaceRef
-          : DEFAULT_SELECT_VALUE,
-      directionSelection: journeyPattern.directionType || DEFAULT_SELECT_VALUE
-    });
-  }
-
-  onFieldChange(field, value) {
-    const { journeyPattern, onChange } = this.props;
-    onChange(journeyPattern.withFieldChange(field, value));
-  }
-
-  handleDirectionSelectionChange(directionSelection) {
-    this.onFieldChange(
-      'directionType',
+  const handleDirectionSelectionChange = directionSelection => {
+    const newDirectionValue =
       directionSelection !== DEFAULT_SELECT_VALUE
         ? directionSelection
-        : undefined
-    );
-    this.setState({ directionSelection });
-  }
+        : undefined;
+    this.onFieldChange('directionType', newDirectionValue);
+    setDirectionSelection(directionSelection);
+  };
 
-  render() {
-    const { journeyPattern, isEditMode, onSave, onClose } = this.props;
-    const { directionSelection } = this.state;
+  return (
+    <div className="journey-pattern-editor">
+      <div className="header">
+        <PageHeader
+          withBackButton
+          onBackButtonClick={onClose}
+          title={`${
+            isEditMode
+              ? formatMessage(messages.edit)
+              : formatMessage(messages.create)
+          }
+            Journey Pattern`}
+        />
 
-    return (
-      <div className="journey-pattern-editor">
-        <div className="header">
-          <PageHeader
-            withBackButton
-            onBackButtonClick={onClose}
-            title={`${isEditMode ? 'Rediger' : 'Opprett'} Journey Pattern`}
-          />
-
-          <div className="header-buttons">
-            <SuccessButton onClick={onSave}>Lagre</SuccessButton>
-          </div>
+        <div className="header-buttons">
+          <SuccessButton onClick={onSave}>
+            {' '}
+            {formatMessage(messages.save)}{' '}
+          </SuccessButton>
         </div>
-        <Tabs>
-          <TabList>
-            <Tab>Generelt</Tab>
-            <Tab>Stoppepunkter</Tab>
-            <Tab>Service Journeys</Tab>
-          </TabList>
-          <TabPanels>
-            <TabPanel>
-              <General
-                journeyPattern={journeyPattern}
-                directionSelection={directionSelection}
-                onFieldChange={this.onFieldChange.bind(this)}
-                handleDirectionSelectionChange={this.handleDirectionSelectionChange.bind(
-                  this
-                )}
-              />
-            </TabPanel>
-            <TabPanel>
-              <StopPointsEditor
-                stopPoints={journeyPattern.pointsInSequence}
-                onChange={pis => this.onFieldChange('pointsInSequence', pis)}
-              />
-            </TabPanel>
-            <TabPanel>
-              <ServiceJourneysEditor
-                serviceJourneys={journeyPattern.serviceJourneys}
-                stopPoints={journeyPattern.pointsInSequence}
-                onChange={sjs => this.onFieldChange('serviceJourneys', sjs)}
-              />
-            </TabPanel>
-          </TabPanels>
-        </Tabs>
       </div>
-    );
-  }
-}
+      <Tabs>
+        <TabList>
+          <Tab>{formatMessage(messages.general)}</Tab>
+          <Tab>{formatMessage(messages.stopPoints)}</Tab>
+          <Tab>{formatMessage(messages.serviceJourneys)}</Tab>
+        </TabList>
+        <TabPanels>
+          <TabPanel>
+            <General
+              journeyPattern={journeyPattern}
+              directionSelection={directionSelection}
+              onFieldChange={onFieldChange.bind(this)}
+              handleDirectionSelectionChange={handleDirectionSelectionChange.bind(
+                this
+              )}
+            />
+          </TabPanel>
+          <TabPanel>
+            <StopPointsEditor
+              stopPoints={pointsInSequence}
+              onChange={pis => onFieldChange('pointsInSequence', pis)}
+            />
+          </TabPanel>
+          <TabPanel>
+            <ServiceJourneysEditor
+              serviceJourneys={serviceJourneys}
+              stopPoints={pointsInSequence}
+              onChange={sjs => onFieldChange('serviceJourneys', sjs)}
+            />
+          </TabPanel>
+        </TabPanels>
+      </Tabs>
+    </div>
+  );
+};
 
 JourneyPatternEditor.defaultProps = {
   journeyPattern: PropTypes.instanceOf(JourneyPattern).isRequired,
