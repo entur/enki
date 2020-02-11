476bec18b42e467751d4cb70899ae9fce3b0987a
Simen Johnsrud <simen.johnsrud92@gmail.com>
2020-02-11 15:44:36 +0100

    Replaced custom checkbox and unused radiobutton with designsystem

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -1,4 +1,1 @@
-export { default as Checkbox } from './Checkbox';
-export { default as Checkmark } from './Checkmark';
 export { default as Loading } from './Loading';
-export { default as RadioButton } from './RadioButton';
