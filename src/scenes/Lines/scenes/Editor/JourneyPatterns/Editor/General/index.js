5a8a4f79fa0934c490dd63efc8e6ba9f064d0dec
Fredrik Meyer <hrmeyer@gmail.com>
2020-02-19 09:41:44 +0100

    Delete unusued import

--- src/scenes/Lines/scenes/Editor/JourneyPatterns/Editor/General/index.js
+++ src/scenes/Lines/scenes/Editor/JourneyPatterns/Editor/General/index.js
@@ -4,7 +4,6 @@ import PropTypes from 'prop-types';
 import { Dropdown } from '@entur/dropdown';
 import { InputGroup, TextArea, TextField } from '@entur/form';
 
-import { JourneyPattern } from 'model';
 import { DIRECTION_TYPE } from 'model/enums';
 import { isBlank } from 'helpers/forms';
 import { DEFAULT_SELECT_VALUE, DEFAULT_SELECT_LABEL } from '../../../constants';
