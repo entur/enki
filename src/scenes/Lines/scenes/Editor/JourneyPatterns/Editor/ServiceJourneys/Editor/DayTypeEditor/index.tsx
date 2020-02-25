3fa39a1e53c356a99746073e1b5c3045dbe46869
Simen Johnsrud <simejo@users.noreply.github.com>
2020-02-25 15:37:07 +0100

    Validation to weekday picker (#114)
    * Converted a few files to typescript
    
    * Added validation for weekdayPicker, cleaned code and added a few tests
    
    * Simplify import
    
    Co-Authored-By: aryani1 <aryan.iranzamini@entur.org>
    
    * Fix validation message
    
    Co-Authored-By: Fredrik Meyer <fredrik.meyer@entur.org>
    
    * Fix validation message
    
    Co-Authored-By: Fredrik Meyer <fredrik.meyer@entur.org>
    
    * Fix validation message
    
    Co-Authored-By: Fredrik Meyer <fredrik.meyer@entur.org>
    
    * Fix validation message
    
    Co-Authored-By: Fredrik Meyer <fredrik.meyer@entur.org>
    
    Co-authored-by: aryani1 <arre_o21@hotmail.com>
    Co-authored-by: Fredrik Meyer <hrmeyer@gmail.com>

---
+++ src/scenes/Lines/scenes/Editor/JourneyPatterns/Editor/ServiceJourneys/Editor/DayTypeEditor/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectIntl } from 'i18n';
+import { Label } from '@entur/typography';
+import { DayType } from 'model';
+import DayTypeAssignmentsEditor from './DayTypeAssignments';
+import WeekdayPicker from 'components/WeekdayPicker';
+import { DAY_OF_WEEK } from 'model/enums';
+import './styles.scss';
+import messages from '../../../messages';
+import DayTypeAssignment from 'model/DayTypeAssignment';
+
+type Props = {
+  dayType?: DayType;
+  onChange: (dayType: DayType) => void;
+};
+
+const DayTypeEditor = ({ dayType, onChange }: Props) => {
+  const { daysOfWeek = [], dayTypeAssignments = [] } = dayType || {};
+  const { formatMessage } = useSelector(selectIntl);
+
+  const onFieldChange = (
+    field: string,
+    value: (DAY_OF_WEEK | DayTypeAssignment)[]
+  ) => {
+    const newDayType = dayType
+      ? dayType.withFieldChange(field, value)
+      : new DayType({ [field]: value });
+
+    onChange(newDayType.isEmpty() ? undefined : newDayType);
+  };
+
+  return (
+    <div className="day-type-editor">
+      <Label> {formatMessage(messages.weekdays)} </Label>
+      <WeekdayPicker
+        days={daysOfWeek}
+        onDaysChange={arr => onFieldChange('daysOfWeek', arr)}
+        feedbackMessage={formatMessage(messages.availabilityMustBeFilled)}
+      />
+      <Label>{formatMessage(messages.dates)}</Label>
+      <DayTypeAssignmentsEditor
+        dayTypeAssignments={dayTypeAssignments}
+        onChange={dayTypeAssignments =>
+          onFieldChange('dayTypeAssignments', dayTypeAssignments)
+        }
+      />
+    </div>
+  );
+};
+
+export default DayTypeEditor;
