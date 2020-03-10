f3ab25804e1492780166f7aa316ad27a03a63303
Aryan Iranzamini <arre_o21@hotmail.com>
2020-03-10 14:11:11 +0100

    Remove unused import

--- src/scenes/Lines/scenes/Editor/JourneyPatterns/Editor/ServiceJourneys/Editor/PassingTimesEditor/index.tsx
+++ src/scenes/Lines/scenes/Editor/JourneyPatterns/Editor/ServiceJourneys/Editor/PassingTimesEditor/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Dropdown } from '@entur/dropdown';
 import { Label } from '@entur/typography';
