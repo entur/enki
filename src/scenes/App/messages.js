506e79ad6272d8d208f7a30421bbabfe6decdb8d
Simen Johnsrud <simen.johnsrud92@gmail.com>
2020-03-24 10:43:12 +0100

    Minimized the introduction page

--- src/scenes/App/messages.js
+++ src/scenes/App/messages.js
@@ -13,47 +13,24 @@ export default defineMessages({
   leadParagraph: {
     id: 'app.home.header.paragraph',
     defaultMessage:
-      'Velkommen til Entur fleksibel transport. Dette er ment som et hjelpende verktøy for at aktører selv skal kunne legge inn fleksible linjetilbud som synliggjøres i våre kanaler. '
+      'Velkommen til Entur fleksibel transport. Dette er et verktøy for at aktører selv skal kunne legge inn fleksible linjetilbud som synliggjøres i våre kanaler.'
   },
   introductionTitle: {
     id: 'app.home.main.section.1.title',
-    defaultMessage: 'Kom igang'
-  },
-  introductionText: {
-    id: 'app.home.main.section.1.paragraph.1',
-    defaultMessage:
-      'Aller første gang du legger inn en fleksibel linje må du starte med å opprette et nettverk til venstre i menyen. Hvis du også ønsker å definere et stoppested eller en sone må du legge inn dette under Stoppesteder til venstre i menyen. '
-  },
-  linesDescription: {
-    id: 'app.home.main.section.1.paragraph.2',
-    defaultMessage:
-      'Når du har laget et nettverk og stoppested/sone er det bare å sette i gang med opprettelse av en ny linje. Dette gjør du under linjer i menyen til venstre. '
-  },
-  summaryTitle: {
-    id: 'app.home.main.section.1.summary',
-    defaultMessage: 'Kort oppsummert'
+    defaultMessage: 'Kom i gang!'
   },
   summaryNetwork: {
     id: 'app.home.main.section.1.summary.item.1',
     defaultMessage:
-      'Lag et nytt nettverk under ‘Nettverk’ i menyen til venstre '
+      '1. Lag et nytt nettverk under ‘Nettverk’ i menyen til venstre '
   },
   summaryStopPoints: {
     id: 'app.home.main.section.1.summary.item.2',
     defaultMessage:
-      'Lag stoppested eller sone under ’Stoppesteder’ i menyen til venstre '
+      '2. Lag stoppested eller sone under ’Stoppesteder’ i menyen til venstre '
   },
   summaryLine: {
     id: 'app.home.main.section.1.summary.item.3',
-    defaultMessage: 'Opprett en ny linje under ‘Linjer’ i menyen til venstre'
-  },
-  lineTypes: {
-    id: 'app.home.main.section.2.title',
-    defaultMessage: 'Ulike linjetyper'
-  },
-  lineTypeDescription: {
-    id: 'app.home.main.section.2.paragraph.1',
-    defaultMessage:
-      'Hvis du ikke er så kjent med de ulike linjetypene du har mulighet til å legge inn, kan du gjerne ta en titt på disse i venstremenyen. Her finner du en oversikt over de ulike typene Enturs reisesøk støtter sammen med en liten beskrivelse av de. '
+    defaultMessage: '3. Opprett en ny linje under ‘Linjer’ i menyen til venstre'
   }
 });
