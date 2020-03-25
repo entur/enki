87327a06e5284aa9cd8ad695a30b7498477ad242
Simen Johnsrud <simejo@users.noreply.github.com>
2020-03-25 08:09:21 +0100

    Minimized the introduction page (#167)

--- src/scenes/App/Home.tsx
+++ src/scenes/App/Home.tsx
@@ -9,25 +9,16 @@ const Home = () => {
   return (
     <div className="home">
       <header>
-        <h4 className="tagline color-blue"> {formatMessage(messages.home)} </h4>
-        <h1> {formatMessage(messages.homeHeader)} </h1>
-        <p className="lead"> {formatMessage(messages.leadParagraph)} </p>
+        <h4 className="tagline color-blue">{formatMessage(messages.home)}</h4>
+        <h1>{formatMessage(messages.homeHeader)}</h1>
+        <p className="lead">{formatMessage(messages.leadParagraph)}</p>
       </header>
       <main>
         <section>
-          <h3> {formatMessage(messages.introductionTitle)} </h3>
-          <p> {formatMessage(messages.introductionText)} </p>
-          <p> {formatMessage(messages.linesDescription)} </p>
-          {formatMessage(messages.summaryTitle)}
-          <ol>
-            <li> {formatMessage(messages.summaryNetwork)} </li>
-            <li> {formatMessage(messages.summaryStopPoints)} </li>
-            <li> {formatMessage(messages.summaryLine)} </li>
-          </ol>
-        </section>
-        <section>
-          <h3> {formatMessage(messages.lineTypes)} </h3>
-          {formatMessage(messages.lineTypeDescription)}
+          <h3>{formatMessage(messages.introductionTitle)}</h3>
+          <p className="point">{formatMessage(messages.summaryNetwork)}</p>
+          <p className="point">{formatMessage(messages.summaryStopPoints)}</p>
+          <p className="point">{formatMessage(messages.summaryLine)}</p>
         </section>
       </main>
     </div>
