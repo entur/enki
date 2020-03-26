234701e3ef1d11d3d7ab0422cfb14fa9bebc2788
Christian H. Mosveen <christian.mosveen@entur.org>
2020-03-26 13:53:04 +0100

    Add tooltip to daytime offset (#175)

--- src/scenes/Lines/scenes/Editor/ServiceJourneys/Editor/PassingTimesEditor/index.tsx
+++ src/scenes/Lines/scenes/Editor/ServiceJourneys/Editor/PassingTimesEditor/index.tsx
@@ -46,6 +46,7 @@ const PassingTimesEditor = (props: Props & StateProps) => {
   const getDayOffsetDropdown = (passingTime: PassingTime, index: number) => (
     <Dropdown
       label={formatMessage(messages.dayTimeOffset)}
+      labelTooltip={formatMessage(messages.dayTimeOffsetTooltip)}
       value={passingTime.departureDayOffset?.toString() ?? '0'}
       items={['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].map(i => ({
         value: i,
