3c2c7fbd6f0b81b7eedb5ac216cf99fd59c49139
Aryan Iranzamini <arre_o21@hotmail.com>
2020-04-06 15:53:13 +0200

    Remove unused files & fix user icon

--- src/scenes/App/components/NavBar/UserPreference/UserMenu/index.tsx
+++ src/scenes/App/components/NavBar/UserPreference/UserMenu/index.tsx
@@ -1,43 +1,22 @@
-import React, { useCallback, useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { updateIntl } from 'react-intl-redux';
-import { RightArrowIcon, UserIcon } from '@entur/icons';
-
-import Menu from 'components/Menu';
-import MenuItem from 'components/Menu/MenuItem';
-import Popover from 'components/Popover';
-import { Checkbox } from '@entur/form';
-import { getMessages, LOCALE_KEY, selectIntl } from 'i18n';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { UserIcon } from '@entur/icons';
 
 import './styles.scss';
 import { GlobalState } from 'reducers';
 import { User } from 'reducers/user';
 
 const UserMenu = () => {
-  const [open, setOpen] = useState(false);
-
-  const { formatMessage, locale } = useSelector(selectIntl);
-  const { familyName, givenName, logoutUrl } = useSelector<GlobalState, User>(
+  const { familyName, givenName } = useSelector<GlobalState, User>(
     (state) => state.user as User
   );
 
-  const dispatch = useDispatch();
-
-  const handleChangeLocale = useCallback(
-    (locale) => {
-      const { messages } = getMessages(locale);
-      dispatch(updateIntl({ locale, messages }));
-      localStorage.setItem(LOCALE_KEY, locale);
-    },
-    [dispatch]
-  );
-
   return (
-    <div className="user-menu" onClick={() => setOpen(true)}>
+    <div className="user-menu">
       <div className="user-icon">
         <UserIcon color="#ffffff" />
       </div>
-      <div className="name">{givenName + ' ' + familyName}</div>
+      <div className="name">{`${givenName} ${familyName}`}</div>
     </div>
   );
 };
