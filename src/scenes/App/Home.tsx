a22dfcec07fdca497b813da4b7d1cf1e563dfc2d
Aryan Iranzamini <arre_o21@hotmail.com>
2020-04-07 10:12:07 +0200

    Fix change of provider issue with cards

--- src/scenes/App/Home.tsx
+++ src/scenes/App/Home.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { selectIntl } from 'i18n';
+import { Link } from 'react-router-dom';
 import { Heading1, LeadParagraph, Heading2 } from '@entur/typography';
 import { NavigationCard } from '@entur/layout';
 import { ChannelsIcon, RulesIcon, MapIcon, BusIcon } from '@entur/icons';
@@ -18,35 +19,41 @@ const Home = () => {
       <main>
         <Heading2>{formatMessage('homeShortcut')}</Heading2>
         <section className="cards">
-          <NavigationCard
-            title={formatMessage('homeCardsGetStarted')}
-            titleIcon={<RulesIcon />}
-            href="/get-started"
-          >
-            {formatMessage('homeCardsGetStartedDescription')}
-          </NavigationCard>
+          <Link to="get-started" className="card-link">
+            <NavigationCard
+              title={formatMessage('homeCardsGetStarted')}
+              titleIcon={<RulesIcon />}
+            >
+              {formatMessage('homeCardsGetStartedDescription')}
+            </NavigationCard>
+          </Link>
 
-          <NavigationCard
-            title={formatMessage('navBarLinesMenuItemLabel')}
-            titleIcon={<BusIcon className="bus-icon" />}
-            href="/lines"
-          >
-            {formatMessage('homeCardsLinesDescription')}
-          </NavigationCard>
-          <NavigationCard
-            title={formatMessage('navBarNetworksMenuItemLabel')}
-            titleIcon={<ChannelsIcon />}
-            href="/networks"
-          >
-            {formatMessage('homeCardsNetworkDescription')}
-          </NavigationCard>
-          <NavigationCard
-            title={formatMessage('navBarStopPlacesMenuItemLabel')}
-            titleIcon={<MapIcon />}
-            href="/stop-places"
-          >
-            {formatMessage('homeCardsStopPlacesDescription')}
-          </NavigationCard>
+          <Link to="/lines" className="card-link">
+            <NavigationCard
+              title={formatMessage('navBarLinesMenuItemLabel')}
+              titleIcon={<BusIcon className="bus-icon" />}
+            >
+              {formatMessage('homeCardsLinesDescription')}
+            </NavigationCard>
+          </Link>
+
+          <Link to="/networks" className="card-link">
+            <NavigationCard
+              title={formatMessage('navBarNetworksMenuItemLabel')}
+              titleIcon={<ChannelsIcon />}
+            >
+              {formatMessage('homeCardsNetworkDescription')}
+            </NavigationCard>
+          </Link>
+
+          <Link to="/stop-places" className="card-link">
+            <NavigationCard
+              title={formatMessage('navBarStopPlacesMenuItemLabel')}
+              titleIcon={<MapIcon />}
+            >
+              {formatMessage('homeCardsStopPlacesDescription')}
+            </NavigationCard>
+          </Link>
         </section>
       </main>
     </div>
