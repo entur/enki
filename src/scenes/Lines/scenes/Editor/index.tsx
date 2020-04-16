d1244eff554acca1181d630a062a7b36d88b6495
Simen Johnsrud <simejo@users.noreply.github.com>
2020-04-16 09:33:34 +0200

    Refactor editor frame (#222)
    * moved navigation buttons to editorFrame where they belong
    
    * Rename index -> FlexibleLineEditor
    
    * Renamed EditorFrame -> index
    
    * Extract steps

--- src/scenes/Lines/scenes/Editor/index.tsx
+++ src/scenes/Lines/scenes/Editor/index.tsx
@@ -1,81 +1,143 @@
-import React, { useState } from 'react';
+import { Stepper } from '@entur/menu';
+import React, { useEffect, useState } from 'react';
+import { useLoadDependencies } from './hooks';
+import { RouteComponentProps } from 'react-router';
+import { MatchParams } from 'http/http';
+import { withRouter } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
+import { selectIntl } from 'i18n';
+import FlexibleLineEditor from 'scenes/Lines/scenes/Editor/FlexibleLineEditor';
+import { GlobalState } from 'reducers';
+import FlexibleLine, { initFlexibleLine } from 'model/FlexibleLine';
+import {
+  filterAuthorities,
+  filterNetexOperators,
+} from 'reducers/organisations';
+import {
+  currentStepIsValid,
+  getMaxAllowedStepIndex,
+  validFlexibleLine,
+} from './validateForm';
+import { isBlank } from 'helpers/forms';
+import { isEmpty } from 'ramda';
+import { getFlexibleLineFromPath } from 'helpers/url';
+import NavigateConfirmBox from 'components/ConfirmNavigationDialog';
+import Loading from 'components/Loading';
+import { setSavedChanges } from 'actions/editor';
+import { loadNetworks, saveNetwork } from 'actions/networks';
+import { Network } from 'model/Network';
+import Provider from 'model/Provider';
+import { PrimaryButton } from '@entur/button';
+import ConfirmDialog from 'components/ConfirmDialog';
+import Page from 'components/Page';
+import NavigationButtons from './NavigationButtons';
 import {
   deleteFlexibleLineById,
   saveFlexibleLine,
 } from 'actions/flexibleLines';
-import OverlayLoader from 'components/OverlayLoader';
-import BookingArrangementEditor from './BookingArrangementEditor';
-import JourneyPatternsEditor from './JourneyPatterns';
-import General from './General';
-import { setSavedChanges } from 'actions/editor';
-import { withRouter } from 'react-router-dom';
-import { selectIntl } from 'i18n';
-import { Organisation } from 'reducers/organisations';
-import { RouteComponentProps } from 'react-router';
-import { MatchParams } from 'http/http';
-import ServiceJourneysEditor from './ServiceJourneys';
-import { changeElementAtIndex } from 'helpers/arrays';
-import NavigationButtons from './NavigationButtons';
-import FlexibleLine from 'model/FlexibleLine';
-import { validJourneyPattern } from './JourneyPatterns/Editor/StopPoints/Editor/validateForm';
-import { validServiceJourneys } from './ServiceJourneys/Editor/validate';
-import { aboutLineStepIsValid } from 'scenes/Lines/scenes/Editor/validateForm';
-import { Network } from 'model/Network';
-import { SmallAlertBox } from '@entur/alert';
+import { FLEXIBLE_LINE_STEPS } from './steps';
 import './styles.scss';
 
-type Props = RouteComponentProps<MatchParams> & {
-  activeStep: number;
-  setActiveStep: (index: number) => void;
-  flexibleLine: FlexibleLine;
-  changeFlexibleLine: (flexibleLine: FlexibleLine) => void;
-  networks: Network[];
-  operators: Organisation[];
-  steps: string[];
-  isEdit: boolean;
-};
+const findNetworkIdByProvider = (
+  provider: Provider,
+  networks: Network[]
+): string | undefined =>
+  networks.find(
+    (network) =>
+      network.id?.split(':')?.[0]?.toUpperCase() === provider.codespace?.xmlns
+  )?.id;
 
-const FlexibleLineEditor = (props: Props) => {
-  const { formatMessage } = useSelector(selectIntl);
+const createAndGetNetwork = (
+  dispatch: any,
+  authorityRef: string,
+  activeProvider: Provider
+): Promise<string> =>
+  dispatch(
+    saveNetwork(
+      {
+        name: activeProvider.codespace?.xmlns ?? 'New network',
+        authorityRef: authorityRef,
+      },
+      false
+    )
+  )
+    .then(() => dispatch(loadNetworks()))
+    .then((newNetworks: Network[]) =>
+      findNetworkIdByProvider(activeProvider, newNetworks)
+    );
+
+const EditorFrame = (props: RouteComponentProps<MatchParams>) => {
+  const [flexibleLine, setFlexibleLine] = useState<FlexibleLine>({});
+  const [showConfirm, setShowConfirm] = useState<boolean>(false);
+  const [nextClicked, setNextClicked] = useState<boolean>(false);
   const [isSaving, setSaving] = useState(false);
   const [isDeleting, setDeleting] = useState(false);
-  const [nextClicked, setNextClicked] = useState<boolean>(false);
+
+  const { formatMessage } = useSelector(selectIntl);
   const dispatch = useDispatch<any>();
+  const {
+    flexibleLines,
+    organisations,
+    networks,
+    editor,
+    providers,
+  } = useSelector<GlobalState, GlobalState>((s) => s);
+
+  const [activeStepperIndex, setActiveStepperIndex] = useState(0);
+
+  const isLoadingDependencies = useLoadDependencies({
+    match: props.match,
+    history: props.history,
+  } as RouteComponentProps<MatchParams>);
+
+  useEffect(() => {
+    if (
+      !isLoadingDependencies &&
+      providers.active &&
+      organisations &&
+      networks
+    ) {
+      const authorities = filterAuthorities(organisations, providers.active);
+      if (!isBlank(props.match.params.id)) {
+        setFlexibleLine(
+          getFlexibleLineFromPath(flexibleLines ?? [], props.match)
+        );
+      } else if (networks.length < 2 && authorities.length > 0) {
+        const networkId = findNetworkIdByProvider(providers.active, networks);
+        if (networkId) {
+          setFlexibleLine(initFlexibleLine(networkId));
+        } else {
+          createAndGetNetwork(
+            dispatch,
+            authorities[0].id,
+            providers.active
+          ).then((networkId) => setFlexibleLine(initFlexibleLine(networkId)));
+        }
+      } else {
+        setFlexibleLine(initFlexibleLine());
+      }
+    }
+    // eslint-disable-next-line
+  }, [flexibleLines, isLoadingDependencies]);
 
   const goToLines = () => props.history.push('/lines');
 
-  const currentStepIsValid = (
-    currentStep: number,
-    flexibleLine: FlexibleLine
-  ) => {
-    if (currentStep === 0) return aboutLineStepIsValid(flexibleLine);
-    else if (currentStep === 1)
-      return validJourneyPattern(flexibleLine.journeyPatterns);
-    else if (currentStep === 2)
-      return validServiceJourneys(
-        flexibleLine.journeyPatterns?.[0].serviceJourneys
-      );
-    else if (currentStep === 3) return true;
-    else return false;
+  const onStepClicked = (stepIndexClicked: number) => {
+    if (getMaxAllowedStepIndex(flexibleLine) >= stepIndexClicked) {
+      setActiveStepperIndex(stepIndexClicked);
+    }
   };
 
-  const invalidSteps = props.steps.filter(
-    (step, i) => !currentStepIsValid(i, props.flexibleLine)
-  );
-
-  const otherStepsHasError =
-    invalidSteps.length > 0 &&
-    !invalidSteps.includes(props.steps[props.activeStep]);
+  const isEdit = !isBlank(props.match.params.id);
 
   const handleOnSaveClick = () => {
-    const valid = invalidSteps.length === 0;
+    const valid = validFlexibleLine(flexibleLine);
 
     setNextClicked(true);
     if (valid) {
       setSaving(true);
-      dispatch(saveFlexibleLine(props.flexibleLine))
-        .then(() => !props.isEdit && goToLines())
+      dispatch(saveFlexibleLine(flexibleLine))
+        .then(() => !isEdit && goToLines())
         .finally(() => setSaving(false));
       dispatch(setSavedChanges(true));
       setNextClicked(false);
@@ -83,130 +145,99 @@ const FlexibleLineEditor = (props: Props) => {
   };
 
   const handleDelete = () => {
-    if (props.flexibleLine.id) {
+    if (flexibleLine.id) {
       setDeleting(true);
-      dispatch(deleteFlexibleLineById(props.flexibleLine.id)).then(() =>
-        goToLines()
-      );
+      dispatch(deleteFlexibleLineById(flexibleLine.id)).then(() => goToLines());
     }
   };
 
   const onNextClicked = () => {
-    if (currentStepIsValid(props.activeStep, props.flexibleLine)) {
-      props.setActiveStep(props.activeStep + 1);
+    if (currentStepIsValid(activeStepperIndex, flexibleLine)) {
+      setActiveStepperIndex(activeStepperIndex + 1);
       setNextClicked(false);
     } else {
       setNextClicked(true);
     }
   };
 
+  const onBackButtonClicked = () =>
+    editor.isSaved ? goToLines() : setShowConfirm(true);
+
+  const onFlexibleLineChange = (flexibleLine: FlexibleLine) => {
+    setFlexibleLine(flexibleLine);
+    dispatch(setSavedChanges(false));
+  };
+
+  const authoritiesMissing =
+    organisations &&
+    filterAuthorities(organisations, providers.active).length === 0;
+
   return (
-    <div className="line-editor">
-      <OverlayLoader
-        className=""
-        isLoading={isSaving || isDeleting}
-        text={
-          isSaving
-            ? formatMessage('editorSaveLineLoadingText')
-            : formatMessage('editorDeleteLineLoadingText')
-        }
-      >
-        <div className="editor-pages">
-          {props.activeStep === 0 && (
-            <>
-              <section className="general-line-info">
-                <General
-                  flexibleLine={props.flexibleLine}
-                  operators={props.operators}
-                  networks={props.networks}
-                  flexibleLineChange={props.changeFlexibleLine}
-                  spoilPristine={nextClicked}
-                />
-              </section>
-            </>
-          )}
-
-          {props.activeStep === 1 && (
-            <section>
-              <JourneyPatternsEditor
-                journeyPatterns={props.flexibleLine.journeyPatterns ?? []}
-                onChange={(jps) =>
-                  props.changeFlexibleLine({
-                    ...props.flexibleLine,
-                    journeyPatterns: jps,
-                  })
-                }
-                flexibleLineType={props.flexibleLine.flexibleLineType}
-                spoilPristine={nextClicked}
-              />
-            </section>
-          )}
-
-          {props.activeStep === 2 && props.flexibleLine.journeyPatterns?.[0] && (
-            <section>
-              <ServiceJourneysEditor
-                serviceJourneys={
-                  props.flexibleLine.journeyPatterns[0].serviceJourneys
-                }
-                stopPoints={
-                  props.flexibleLine.journeyPatterns[0].pointsInSequence
-                }
-                spoilPristine={nextClicked}
-                flexibleLineType={props.flexibleLine.flexibleLineType}
-                onChange={(sjs) =>
-                  props.changeFlexibleLine({
-                    ...props.flexibleLine,
-                    journeyPatterns: changeElementAtIndex(
-                      props.flexibleLine.journeyPatterns!,
-                      {
-                        ...props.flexibleLine.journeyPatterns![0],
-                        serviceJourneys: sjs,
-                      },
-                      0
-                    ),
-                  })
-                }
-              />
-            </section>
-          )}
-
-          {props.activeStep === 3 && (
-            <section>
-              <BookingArrangementEditor
-                bookingArrangement={props.flexibleLine.bookingArrangement ?? {}}
-                onChange={(b) =>
-                  props.changeFlexibleLine({
-                    ...props.flexibleLine,
-                    bookingArrangement: b,
-                  })
-                }
-                spoilPristine={nextClicked}
-              />
-            </section>
-          )}
-
-          {otherStepsHasError && nextClicked && props.isEdit && (
-            <SmallAlertBox
-              className="step-errors"
-              variant="error"
-              width="fit-content"
-            >
-              {formatMessage('fixErrorsInTheFollowingSteps')}
-              {invalidSteps.join(', ')}
-            </SmallAlertBox>
-          )}
-
-          <NavigationButtons
-            editMode={props.isEdit}
-            lastStep={props.activeStep === props.steps.length - 1}
-            onDelete={handleDelete}
-            onSave={handleOnSaveClick}
-            onNext={onNextClicked}
+    <Page
+      backButtonTitle={formatMessage('navBarLinesMenuItemLabel')}
+      onBackButtonClick={onBackButtonClicked}
+    >
+      <>
+        <Loading
+          className=""
+          isLoading={isLoadingDependencies || isEmpty(flexibleLine)}
+          text={formatMessage('editorLoadingLineText')}
+        >
+          <>
+            <Stepper
+              className="editor-frame"
+              steps={FLEXIBLE_LINE_STEPS.map((step) => formatMessage(step))}
+              activeIndex={activeStepperIndex}
+              onStepClick={(index) => onStepClicked(index)}
+            />
+            <FlexibleLineEditor
+              isEdit={isEdit}
+              activeStep={activeStepperIndex}
+              setActiveStep={setActiveStepperIndex}
+              flexibleLine={flexibleLine}
+              changeFlexibleLine={onFlexibleLineChange}
+              operators={filterNetexOperators(organisations ?? [])}
+              networks={networks ?? []}
+              spoilPristine={nextClicked}
+              isSaving={isSaving}
+              isDeleting={isDeleting}
+            />
+            <NavigationButtons
+              editMode={isEdit}
+              lastStep={activeStepperIndex === FLEXIBLE_LINE_STEPS.length - 1}
+              onDelete={handleDelete}
+              onSave={handleOnSaveClick}
+              onNext={onNextClicked}
+            />
+          </>
+        </Loading>
+        {showConfirm && (
+          <NavigateConfirmBox
+            hideDialog={() => setShowConfirm(false)}
+            redirectTo="/lines"
+            title={formatMessage('redirectTitle')}
+            description={formatMessage('redirectMessage')}
+            confirmText={formatMessage('redirectYes')}
+            cancelText={formatMessage('redirectNo')}
           />
-        </div>
-      </OverlayLoader>
-    </div>
+        )}
+        {authoritiesMissing && (
+          <ConfirmDialog
+            className="authority-missing-modal"
+            isOpen={true}
+            title={formatMessage('networkAuthorityMissing')}
+            message={formatMessage('networkAuthorityMissingDetails')}
+            onDismiss={() => props.history.push('/')}
+            buttons={[
+              <PrimaryButton key="0" onClick={() => props.history.push('/')}>
+                {formatMessage('homePage')}
+              </PrimaryButton>,
+            ]}
+          />
+        )}
+      </>
+    </Page>
   );
 };
 
-export default withRouter(FlexibleLineEditor);
+export default withRouter(EditorFrame);
