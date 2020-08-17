810d807341a7ab85ac9c10736fafb0545e63378d
Tom Erik Støwer <testower@gmail.com>
2020-08-17 10:42:20 +0200

    Split model types

--- src/model/FlexibleLine.ts
+++ src/model/FlexibleLine.ts
@@ -1,41 +1,13 @@
 import BookingArrangement from './BookingArrangement';
-import JourneyPattern, {
-  initJourneyPatterns,
-  journeyPatternToPayload,
-} from './JourneyPattern';
-import Notice from './Notice';
-import { Network } from './Network';
-import VersionedType from 'model/VersionedType';
-import { VEHICLE_MODE, VEHICLE_SUBMODE } from 'model/enums';
+import Line, { initLine, lineToPayload } from './Line';
 
-type FlexibleLine = VersionedType & {
-  name?: string;
-  description?: string;
-  privateCode?: string;
-  publicCode?: string;
-  transportMode?: VEHICLE_MODE;
-  transportSubmode?: VEHICLE_SUBMODE;
+type FlexibleLine = Line & {
   flexibleLineType?: string;
-  network?: Network;
-  networkRef?: string; // Either of network or networkRef seems useless / duplicated
-  operatorRef?: string;
   bookingArrangement?: BookingArrangement;
-  journeyPatterns?: JourneyPattern[];
-  notices?: Notice[];
 };
 
-export const initFlexibleLine = () => ({
-  journeyPatterns: initJourneyPatterns(),
-});
+export const initFlexibleLine = initLine;
 
-export const flexibleLineToPayload = (flexibleLine: FlexibleLine) => {
-  const { network, ...rest } = flexibleLine;
-  return {
-    ...rest,
-    journeyPatterns: flexibleLine.journeyPatterns?.map((journeyPattern) =>
-      journeyPatternToPayload(journeyPattern)
-    ),
-  };
-};
+export const flexibleLineToPayload = lineToPayload;
 
 export default FlexibleLine;
---
+++ src/model/Line.ts
@@ -0,0 +1,38 @@
+import JourneyPattern, {
+  initJourneyPatterns,
+  journeyPatternToPayload,
+} from './JourneyPattern';
+import Notice from './Notice';
+import { Network } from './Network';
+import VersionedType from 'model/VersionedType';
+import { VEHICLE_MODE, VEHICLE_SUBMODE } from 'model/enums';
+
+type Line = VersionedType & {
+  name?: string;
+  description?: string;
+  privateCode?: string;
+  publicCode?: string;
+  transportMode?: VEHICLE_MODE;
+  transportSubmode?: VEHICLE_SUBMODE;
+  network?: Network;
+  networkRef?: string; // Either of network or networkRef seems useless / duplicated
+  operatorRef?: string;
+  journeyPatterns?: JourneyPattern[];
+  notices?: Notice[];
+};
+
+export const initLine = () => ({
+  journeyPatterns: initJourneyPatterns(),
+});
+
+export const lineToPayload = (line: Line) => {
+  const { network, ...rest } = line;
+  return {
+    ...rest,
+    journeyPatterns: line.journeyPatterns?.map((journeyPattern) =>
+      journeyPatternToPayload(journeyPattern)
+    ),
+  };
+};
+
+export default Line;
