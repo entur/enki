cb9e513d411224b9e452a5a207391137405bef11
Tom Erik Støwer <testower@gmail.com>
2020-09-21 21:19:57 +0200

    Remove unused dependencies and move rest
    No need to distinguish between dev deps and regular deps

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'normalize.css';
 import { Provider } from 'react-intl-redux';
 import Keycloak, { KeycloakInstance } from 'keycloak-js';
 
