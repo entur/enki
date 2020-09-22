154cc6c0ae1065e8408f14e0b9c3d7dfc077b4b7
Tom Erik Støwer <testower@gmail.com>
2020-09-22 12:47:16 +0200

    WTK-25 More tests for LinesForExport, clicking checkboxes

--- src/components/LinesForExport/LinesForExport.test.tsx
+++ src/components/LinesForExport/LinesForExport.test.tsx
@@ -1,12 +1,21 @@
 import React from 'react';
-import { render, act } from 'utils/test-utils';
 import { MockedProvider } from '@apollo/client/testing';
-
-import LinesForExport from '.';
 import { MemoryRouter } from 'react-router-dom';
-
-import { GET_LINES_FOR_EXPORT } from 'api/uttu/queries';
 import { format, addDays, subDays } from 'date-fns';
+
+import {
+  render,
+  act,
+  getByText,
+  RenderResult,
+  getAllByRole,
+  fireEvent,
+} from 'utils/test-utils';
+import { GET_LINES_FOR_EXPORT } from 'api/uttu/queries';
+import { ExportLineAssociation } from 'model/Export';
+import LinesForExport from '.';
+
+import '@testing-library/jest-dom/extend-expect';
 
 const line = {
   id: 'TST:Line:1',
@@ -97,25 +106,86 @@ const mocks = [
   },
 ];
 
-it('renders without crashing', async () => {
-  render(
-    <MockedProvider
-      mocks={mocks}
-      defaultOptions={{
-        watchQuery: { fetchPolicy: 'no-cache' },
-        query: { fetchPolicy: 'no-cache' },
-      }}
-    >
-      <MemoryRouter>
-        <LinesForExport
-          fromDate={format(new Date(), 'yyyy-MM-dd')}
-          toDate={format(addDays(new Date(), 120), 'yyyy-MM-dd')}
-          onChange={() => {}}
-        />
-      </MemoryRouter>
-    </MockedProvider>
-  );
+const wait = async () => {
   await act(async () => {
     await new Promise((resolve) => setTimeout(resolve, 0));
+  });
+};
+
+const clickCheckbox = (container: HTMLElement, index: number) => {
+  fireEvent(
+    getAllByRole(container, 'checkbox')[index],
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+    })
+  );
+};
+
+const lineAssociations: ExportLineAssociation[] = [
+  { lineRef: 'TST:Line:1' },
+  { lineRef: 'TST:Line:2' },
+  { lineRef: 'TST:FlexibleLine:1' },
+];
+
+describe('LinesForExport', () => {
+  let renderResult: RenderResult;
+  let mockedOnChange: jest.Mock<any, any>;
+  beforeEach(() => {
+    mockedOnChange = jest.fn();
+    renderResult = render(
+      <MockedProvider
+        mocks={mocks}
+        defaultOptions={{
+          watchQuery: { fetchPolicy: 'no-cache' },
+          query: { fetchPolicy: 'no-cache' },
+        }}
+      >
+        <MemoryRouter>
+          <LinesForExport
+            fromDate={format(new Date(), 'yyyy-MM-dd')}
+            toDate={format(addDays(new Date(), 120), 'yyyy-MM-dd')}
+            onChange={mockedOnChange}
+          />
+        </MemoryRouter>
+      </MockedProvider>
+    );
+  });
+
+  it('renders without crashing', async () => {
+    await act(async () => {
+      await new Promise((resolve) => setTimeout(resolve, 0));
+    });
+
+    expect(getByText(renderResult.container, 'Test line')).toBeInTheDocument();
+    expect(getByText(renderResult.container, 'TST:Line:1')).toBeInTheDocument();
+  });
+
+  it('calls onChange with correct lines when selecting/deselecting all', async () => {
+    expect(mockedOnChange).toHaveBeenCalledWith([]);
+
+    await wait();
+
+    expect(mockedOnChange).toHaveBeenCalledWith(lineAssociations);
+
+    clickCheckbox(renderResult.container, 0);
+
+    expect(mockedOnChange).toHaveBeenCalledWith([]);
+
+    clickCheckbox(renderResult.container, 0);
+
+    expect(mockedOnChange).toHaveBeenCalledWith(lineAssociations);
+  });
+
+  it('calls onChange with correct lines when selecting deselecting single checkbox', async () => {
+    expect(mockedOnChange).toHaveBeenCalledWith([]);
+
+    await wait();
+
+    expect(mockedOnChange).toHaveBeenCalledWith(lineAssociations);
+
+    clickCheckbox(renderResult.container, 1);
+
+    expect(mockedOnChange).toHaveBeenCalledWith(lineAssociations.slice(1));
   });
 });
