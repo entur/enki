f0fcf1a70109cd41a2936f28b75039982318813b
Tom Erik Støwer <testower@gmail.com>
2021-05-07 14:41:05 +0200

    WTK-435 Remove special treatment of arrival / departure time in flexible line received reducer

--- src/reducers/flexibleLines.ts
+++ src/reducers/flexibleLines.ts
@@ -4,43 +4,10 @@ import {
   FlexibleLinesAction,
 } from 'actions/flexibleLines';
 import FlexibleLine from '../model/FlexibleLine';
-import PassingTime from 'model/PassingTime';
 import JourneyPattern from 'model/JourneyPattern';
 import { SET_ACTIVE_PROVIDER } from 'actions/providers';
 
 export type FlexibleLinesState = FlexibleLine[] | null;
-
-const cleanUpPassingTimes = (pt: PassingTime): PassingTime => {
-  if (pt.departureTime)
-    return {
-      departureTime: pt.departureTime,
-      arrivalTime: pt.departureTime,
-      departureDayOffset: pt.departureDayOffset,
-      arrivalDayOffset: pt.departureDayOffset,
-    };
-  else if (pt.latestArrivalTime)
-    return {
-      departureTime: pt.latestArrivalTime,
-      arrivalTime: pt.latestArrivalTime,
-      departureDayOffset: pt.latestArrivalDayOffset,
-      arrivalDayOffset: pt.latestArrivalDayOffset,
-    };
-  else if (pt.arrivalTime)
-    return {
-      departureTime: pt.arrivalTime,
-      arrivalTime: pt.arrivalTime,
-      departureDayOffset: pt.arrivalDayOffset,
-      arrivalDayOffset: pt.arrivalDayOffset,
-    };
-  else if (pt.earliestDepartureTime)
-    return {
-      departureTime: pt.earliestDepartureTime,
-      arrivalTime: pt.earliestDepartureTime,
-      departureDayOffset: pt.earliestDepartureDayOffset,
-      arrivalDayOffset: pt.earliestDepartureDayOffset,
-    };
-  else return {};
-};
 
 const flexibleLines = (
   lines: FlexibleLinesState = null,
@@ -57,10 +24,6 @@ const flexibleLines = (
           pointsInSequence: jp.pointsInSequence.map((pis) => ({
             ...pis,
             flexibleStopPlaceRef: pis.flexibleStopPlace?.id,
-          })),
-          serviceJourneys: jp.serviceJourneys.map((sj) => ({
-            ...sj,
-            passingTimes: sj.passingTimes.map((pt) => cleanUpPassingTimes(pt)),
           })),
         })) ?? [];
 
