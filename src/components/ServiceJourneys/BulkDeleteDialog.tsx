3c1cdaabc329e0e791e769747932743289a3c1e8
Tom Erik Støwer <testower@gmail.com>
2021-05-31 20:08:02 +0200

    WTK-456 Add space above buttons

--- src/components/ServiceJourneys/BulkDeleteDialog.tsx
+++ src/components/ServiceJourneys/BulkDeleteDialog.tsx
@@ -117,7 +117,7 @@ export default (props: Props) => {
         </TableBody>
       </Table>
 
-      <ButtonGroup>
+      <ButtonGroup style={{ marginTop: '1rem' }}>
         <SecondaryButton onClick={() => dismiss()}>
           {formatMessage('bulkDeleteDialogCancelButtonLabel')}
         </SecondaryButton>
