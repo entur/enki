bf775920c3476bd1bd90016863ad60d8f3a3e137
Tom Erik Støwer <testower@gmail.com>
2022-05-02 12:19:59 +0200

    Get organisations via uttu

--- src/actions/organisations.ts
+++ src/actions/organisations.ts
@@ -1,6 +1,7 @@
 import http from 'http/http';
 import { OrganisationState } from 'reducers/organisations';
 import { ORGANISATION_TYPE } from 'model/enums';
+import { GlobalState } from 'reducers';
 
 export const RECEIVE_ORGANISATIONS = 'RECEIVE_ORGANISATIONS';
 
@@ -16,12 +17,21 @@ export const receiveOrganisations = (
   organisations,
 });
 
-export const getOrganisations = (types?: ORGANISATION_TYPE[]) => (
-  dispatch: (receiveOrganisations: ReceiveOrganisations) => void
-) => {
-  const typesQuery = types?.length ? '?types' + types.join(',') : '';
-  return http.get(`/organisations${typesQuery}`).then((response) => {
-    dispatch(receiveOrganisations(response.data));
-    return Promise.resolve(response.data);
-  });
-};
+export const getOrganisations =
+  (types?: ORGANISATION_TYPE[]) =>
+  async (
+    dispatch: (receiveOrganisations: ReceiveOrganisations) => void,
+    getState: () => GlobalState
+  ) => {
+    const typesQuery = types?.length ? '?types' + types.join(',') : '';
+    return http
+      .get(`/organisations${typesQuery}`, {
+        headers: {
+          Authorization: `Bearer ${await getState().auth.getAccessToken()}`,
+        },
+      })
+      .then((response) => {
+        dispatch(receiveOrganisations(response.data));
+        return Promise.resolve(response.data);
+      });
+  };
