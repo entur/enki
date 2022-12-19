d9d23a20edec7f0f4b19e52b7e92068bee6b72e9
Tom Erik Støwer <testower@gmail.com>
2022-12-19 20:17:23 +0100

    Remove unused import

--- src/components/ServiceJourneyEditor/PassingTimesEditor/PassingTimeEditor.tsx
+++ src/components/ServiceJourneyEditor/PassingTimesEditor/PassingTimeEditor.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useState } from 'react';
 import { Radio, RadioGroup } from '@entur/form';
 import PassingTime from 'model/PassingTime';
 import { TimePicker } from '@entur/datepicker';
