4f81159d820cb265226c65b47fa91cc1adcc0857
Tom Erik Støwer <testower@gmail.com>
2023-01-26 22:00:20 +0100

    Fix configs

--- server/src/config/app.js
+++ server/src/config/app.js
@@ -1,3 +1,4 @@
 module.exports = {
   enableLegacyOrganisationsFilter: process.env.ENABLE_LEGACY_ORGANISATIONS_FILTER === 'true',
+  adminRole: process.env.ADMIN_ROLE,
 }
--- src/config/fetchConfig.ts
+++ src/config/fetchConfig.ts
@@ -1,8 +1,4 @@
 export const fetchConfig = async (apiBase?: string) => {
   const resp = await fetch(`${apiBase}/app.json`);
-  const config = await resp.json();
-  return {
-    ...config,
-    adminRole: process.env.REACT_APP_ADMIN_ROLE,
-  };
+  return resp.json();
 };
