b3ef26af006ea831c8b52285e0f1285a372fa954
Tom Erik Støwer <testower@gmail.com>
2023-01-30 11:20:27 +0100

    Prepare for firebase deployment

--- server/src/router/routes/app.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const app = require('../../config/app');
-
-router.get('/app.json', (req, res) => {
-  res.json(app);
-});
-
-module.exports = router;
--- server/src/router/routes/auth.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const auth0Config = require('../../config/auth0');
-
-router.get('/auth0.json', (req, res) => {
-  res.json(auth0Config);
-});
-
-module.exports = router;
