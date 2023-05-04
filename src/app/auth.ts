8c2490c28d72427670860dcb06caa3fbb70cdc1b
Tom Erik Støwer <testower@gmail.com>
2023-05-04 13:22:11 +0200

    Use AuthProvider component from react-oidc-context directly

--- src/app/auth.ts
+++ src/app/auth.ts
@@ -1,11 +1,6 @@
 import { useConfig } from 'config/ConfigContext';
-import { OidcClientSettings, User as OidcUser } from 'oidc-client-ts';
-import React, { useEffect } from 'react';
-import {
-  AuthProvider as OidcAuthProvider,
-  hasAuthParams,
-  useAuth as useOidcAuth,
-} from 'react-oidc-context';
+import { useEffect } from 'react';
+import { hasAuthParams, useAuth as useOidcAuth } from 'react-oidc-context';
 
 export interface Auth<T extends User> {
   isLoading: boolean;
@@ -71,20 +66,3 @@ export const useAuth = (): Auth<User> => {
     roleAssignments: user?.profile[claimsNamespace!] as string[],
   };
 };
-
-type Props = {
-  children: React.ReactNode;
-  oidcConfig?: OidcClientSettings;
-};
-
-export const AuthProvider: React.FC<Props> = ({ children, oidcConfig }) => (
-  <OidcAuthProvider
-    {...oidcConfig!}
-    onSigninCallback={(_user: OidcUser | void): void => {
-      window.history.replaceState({}, document.title, window.location.pathname);
-    }}
-    redirect_uri={window.location.origin}
-  >
-    {children}
-  </OidcAuthProvider>
-);
--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom/client';
 import App from 'scenes/App';
 
 import * as Sentry from '@sentry/react';
-import { AuthProvider, useAuth } from 'app/auth';
+import { useAuth } from 'app/auth';
 import { useAppDispatch, useAppSelector } from 'app/hooks';
 import { store } from 'app/store';
 import { getEnvironment } from 'config/getEnvironment';
@@ -13,6 +13,7 @@ import { selectAuthLoaded, updateAuth } from 'features/app/authSlice';
 import { selectConfigLoaded, updateConfig } from 'features/app/configSlice';
 import { normalizeAllUrls } from 'helpers/url';
 import { EnkiIntlProvider } from 'i18n/EnkiIntlProvider';
+import { AuthProvider } from 'react-oidc-context';
 import { Provider } from 'react-redux';
 import './styles/index.scss';
 
@@ -59,16 +60,24 @@ const AuthenticatedApp = () => {
 const renderIndex = async () => {
   const container = document.getElementById('root');
   const root = ReactDOM.createRoot(container!);
-
   const config = await fetchConfig();
 
-  const { oidcConfig } = config;
   root.render(
     <Sentry.ErrorBoundary>
       <Provider store={store}>
         <EnkiIntlProvider>
           <ConfigContext.Provider value={config}>
-            <AuthProvider oidcConfig={oidcConfig}>
+            <AuthProvider
+              {...config.oidcConfig!}
+              onSigninCallback={() => {
+                window.history.replaceState(
+                  {},
+                  document.title,
+                  window.location.pathname
+                );
+              }}
+              redirect_uri={window.location.origin}
+            >
               <AuthenticatedApp />
             </AuthProvider>
           </ConfigContext.Provider>
