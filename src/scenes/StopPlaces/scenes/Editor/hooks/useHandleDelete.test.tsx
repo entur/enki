40d167ece7da9b46f3e20846b6da0f94b2a6f357
Tom Erik Støwer <testower@gmail.com>
2023-08-11 13:19:56 +0200

    Expand test

--- src/scenes/StopPlaces/scenes/Editor/hooks/useHandleDelete.test.tsx
+++ src/scenes/StopPlaces/scenes/Editor/hooks/useHandleDelete.test.tsx
@@ -1,4 +1,5 @@
-import { renderHook } from '@testing-library/react';
+import { act, renderHook } from '@testing-library/react';
+import * as actions from 'actions/flexibleStopPlaces';
 import { store } from 'app/store';
 import FlexibleStopPlace from 'model/FlexibleStopPlace';
 import { IntlProvider } from 'react-intl';
@@ -6,29 +7,73 @@ import { Provider } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 import { useHandleDelete } from './useHandleDelete';
 
-it('handeDelete with stop place without id does not attempt to save', () => {
-  const onCall = jest.fn();
-  const onSaveStart = jest.fn();
-  const onSaveEnd = jest.fn();
+function sleep(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
-  const testSubject: FlexibleStopPlace = {};
+describe('useHandleDelete', () => {
+  it('with stop place without id does not attempt to save', () => {
+    const onCall = jest.fn();
+    const onSaveStart = jest.fn();
+    const onSaveEnd = jest.fn();
 
-  const { result } = renderHook(
-    () => useHandleDelete(testSubject, onCall, onSaveStart, onSaveEnd),
-    {
-      wrapper: ({ children }) => (
-        <Provider store={store}>
-          <MemoryRouter>
-            <IntlProvider locale="en">{children}</IntlProvider>
-          </MemoryRouter>
-        </Provider>
-      ),
-    }
-  );
+    const testSubject: FlexibleStopPlace = {};
 
-  result.current();
+    const { result } = renderHook(
+      () => useHandleDelete(testSubject, onCall, onSaveStart, onSaveEnd),
+      {
+        wrapper: ({ children }) => (
+          <Provider store={store}>
+            <MemoryRouter>
+              <IntlProvider locale="en">{children}</IntlProvider>
+            </MemoryRouter>
+          </Provider>
+        ),
+      }
+    );
 
-  expect(onCall).toBeCalledTimes(1);
-  expect(onSaveStart).toBeCalledTimes(0);
-  expect(onSaveEnd).toBeCalledTimes(0);
+    result.current();
+
+    expect(onCall).toBeCalledTimes(1);
+    expect(onSaveStart).toBeCalledTimes(0);
+    expect(onSaveEnd).toBeCalledTimes(0);
+  });
+
+  it('with stop place with id works', async () => {
+    // @ts-ignore
+    actions.deleteFlexibleStopPlaceById = jest
+      .fn()
+      .mockReturnValueOnce(() => Promise.resolve());
+
+    const onCall = jest.fn();
+    const onSaveStart = jest.fn();
+    const onSaveEnd = jest.fn();
+
+    const testSubject: FlexibleStopPlace = {
+      id: 'test',
+    };
+
+    const { result } = renderHook(
+      () => useHandleDelete(testSubject, onCall, onSaveStart, onSaveEnd),
+      {
+        wrapper: ({ children }) => (
+          <Provider store={store}>
+            <MemoryRouter>
+              <IntlProvider locale="en">{children}</IntlProvider>
+            </MemoryRouter>
+          </Provider>
+        ),
+      }
+    );
+
+    result.current();
+
+    expect(onCall).toBeCalledTimes(1);
+    expect(onSaveStart).toBeCalledTimes(1);
+
+    // Is there a better way?
+    await act(() => sleep(0));
+
+    expect(onSaveEnd).toBeCalledTimes(1);
+  });
 });
