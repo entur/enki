1f8bf9fb040e8d740b000563068795c77bb81788
Tom Erik Støwer <testower@gmail.com>
2023-08-22 11:33:50 +0200

    Add file that escaped commit

---
+++ src/scenes/StopPlaces/findFlexibleStopAreaType.ts
@@ -0,0 +1,9 @@
+import { KeyValues } from 'model/KeyValues';
+import { FLEXIBLE_STOP_AREA_TYPE } from 'model/enums';
+
+export const findFlexibleStopAreaType = (
+  keyValues?: KeyValues[]
+): FLEXIBLE_STOP_AREA_TYPE | undefined =>
+  keyValues?.find((v) => v.key === 'FlexibleStopAreaType')?.values[0] as
+    | FLEXIBLE_STOP_AREA_TYPE
+    | undefined;
