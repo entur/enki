4825f3e3b90af63fb489242a455bab2aaffafb76
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-04-18 10:46:28 +0200

    Add documentation to config parameters

--- src/config/ConfigContext.ts
+++ src/config/ConfigContext.ts
@@ -3,14 +3,59 @@ import { OidcClientSettings } from 'oidc-client-ts';
 import { createContext, useContext } from 'react';
 
 export interface Config {
+  /**
+   * Base URL for backend GraphQL API (a.k.a. uttu)
+   *
+   * Note that uttu has multiple GraphQL endpoints, one for each
+   * provider and additionally one for the providers API.
+   */
   uttuApiUrl?: string;
-  enableLegacyOrganisationsFilter?: boolean;
-  adminRole?: string;
-  xmlnsUrlPrefix?: string;
-  preferredNameNamespace?: string;
-  claimsNamespace?: string;
+
+  /**
+   * Configure OpenID Connect for authenticating users with a compliant authentication provider
+   *
+   * {@see https://authts.github.io/oidc-client-ts/interfaces/OidcClientSettings.html}
+   */
   oidcConfig?: OidcClientSettings;
+
+  /**
+   * Prefix used in XML namespace for providers in exported datasets
+   */
+  xmlnsUrlPrefix?: string;
+
+  /**
+   * Enables Entur specific legacy bevaior for filtering authorities and operators.
+   * {@see ../model/Organisation.ts}
+   */
+  enableLegacyOrganisationsFilter?: boolean;
+
+  /**
+   * Optionally restrict available flexible line types available for users to choose from when
+   * creating flexible lines.
+   */
   supportedFlexibleLineTypes?: FlexibleLineType[];
+
+  /**
+   * The exact shape of the admin role used to match against role claims in token, toggles
+   * visibility of providers admin menu option.
+   *
+   * This is technical debt, and will be moved to the backend
+   */
+  adminRole?: string;
+
+  /**
+   * Namespace for preferred name in tokens
+   *
+   * This is technical debt, and will be moved to the backend
+   */
+  preferredNameNamespace?: string;
+
+  /**
+   * Namespace for claims in tokens
+   *
+   * This is technical debt, and will be moved to the backend
+   */
+  claimsNamespace?: string;
 }
 
 export const ConfigContext = createContext<Config>({});
