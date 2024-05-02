0e238cf1669228bd8c5fb4a60f6e6fbffffb6234
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-05-02 22:39:39 +0200

    Remove last usage of providers state outside of providers screen

--- src/scenes/App/useNoProviders.ts
+++ src/scenes/App/useNoProviders.ts
@@ -2,13 +2,12 @@ import { useLocation } from 'react-router-dom';
 import { useAppSelector } from '../../store/hooks';
 
 export const useNoProviders = () => {
-  const providers = useAppSelector((state) => state.providers);
+  const providers = useAppSelector((state) => state.userContext.providers);
   const isAdmin = useAppSelector((state) => state.userContext.isAdmin);
   const location = useLocation();
 
   return (
-    providers.providers &&
-    providers.providers.length === 0 &&
+    providers.length === 0 &&
     isAdmin &&
     location.pathname !== '/providers' &&
     location.pathname !== '/providers/create'
