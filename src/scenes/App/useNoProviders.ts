a32b43bcd779ccd16b166736f0b3f9fee108f8b9
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-05-02 12:42:34 +0200

    Update useNoProviders hook

--- src/scenes/App/useNoProviders.ts
+++ src/scenes/App/useNoProviders.ts
@@ -1,18 +1,15 @@
-import { useConfig } from 'config/ConfigContext';
 import { useLocation } from 'react-router-dom';
-import { useAuth } from '../../auth/auth';
 import { useAppSelector } from '../../store/hooks';
 
 export const useNoProviders = () => {
   const providers = useAppSelector((state) => state.providers);
-  const auth = useAuth();
-  const { adminRole } = useConfig();
+  const isAdmin = useAppSelector((state) => state.userContext.isAdmin);
   const location = useLocation();
 
   return (
     providers.providers &&
     providers.providers.length === 0 &&
-    auth.roleAssignments?.includes(adminRole!) &&
+    isAdmin &&
     location.pathname !== '/providers' &&
     location.pathname !== '/providers/create'
   );
