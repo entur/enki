65c0a1d1ab06608c97bcb121a65098e794f92958
Esko Suomi <esko.suomi@solita.fi>
2024-06-07 10:22:04 +0300

    make bootstrap.json url base path aware

--- src/config/fetchConfig.ts
+++ src/config/fetchConfig.ts
@@ -12,8 +12,11 @@ export const fetchConfig = async (): Promise<Config> => {
   if (import.meta.env.VITE_REACT_APP_UTTU_API_URL) {
     overrides.uttuApiUrl = import.meta.env.VITE_REACT_APP_UTTU_API_URL;
   }
-
-  const response = await fetch('/bootstrap.json');
+  let input = '/bootstrap.json';
+  if (import.meta.env.BASE_URL) {
+    input = import.meta.env.BASE_URL + input;
+  }
+  const response = await fetch(input);
   const config: Config = await response.json();
 
   return Object.assign({}, defaultConfig, config, overrides);
