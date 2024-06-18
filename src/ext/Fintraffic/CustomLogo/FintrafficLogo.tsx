dfa85d7ba00b2612bdbe9ed3706c64cba128d0bc
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-06-18 09:26:59 +0200

    Use url loader for stylesheets and load them via helmet

--- src/ext/Fintraffic/CustomLogo/FintrafficLogo.tsx
+++ src/ext/Fintraffic/CustomLogo/FintrafficLogo.tsx
@@ -1,19 +1,25 @@
-import './styles.scss';
+import stylesURL from './styles.scss?url';
 import { useIntl } from 'react-intl';
 import logo from './logo.png';
 import React from 'react';
+import { Helmet } from 'react-helmet';
 
 export const FintrafficLogo = () => {
   const { formatMessage } = useIntl();
 
   return (
-    <div className="custom-logo-wrapper">
-      <img
-        className="logo"
-        src={logo}
-        alt={formatMessage({ id: 'navBarRootLinkLogoAltText' })}
-      />
-      <div>{formatMessage({ id: 'appLogoTitle' })}</div>
-    </div>
+    <>
+      <Helmet>
+        <link href={stylesURL} rel="stylesheet" media="all" />;
+      </Helmet>
+      <div className="custom-logo-wrapper">
+        <img
+          className="logo"
+          src={logo}
+          alt={formatMessage({ id: 'navBarRootLinkLogoAltText' })}
+        />
+        <div>{formatMessage({ id: 'appLogoTitle' })}</div>
+      </div>
+    </>
   );
 };
--- src/ext/Fintraffic/CustomStyle/FintrafficStyle.tsx
+++ src/ext/Fintraffic/CustomStyle/FintrafficStyle.tsx
@@ -1,8 +1,13 @@
-import './styles.scss';
+import stylesURL from './styles.scss?url';
+import { Helmet } from 'react-helmet';
 
 /**
  * Main point in here is just to import the style overrides
  */
 export const FintrafficStyle = () => {
-  return <></>;
+  return (
+    <Helmet>
+      <link href={stylesURL} rel="stylesheet" media="all" />;
+    </Helmet>
+  );
 };
