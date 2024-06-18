0510a76643dc61f9afc6e99565a7aeb5d5041fd2
Tom Erik Støwer <tom.erik.stower@entur.org>
2024-06-18 13:13:51 +0200

    Cleanup

--- src/ext/Fintraffic/CustomLogo/FintrafficLogo.tsx
+++ src/ext/Fintraffic/CustomLogo/FintrafficLogo.tsx
@@ -10,7 +10,7 @@ export const FintrafficLogo = () => {
   return (
     <>
       <Helmet>
-        <link href={stylesURL} rel="stylesheet" media="all" />;
+        <link href={stylesURL} rel="stylesheet" media="all" />
       </Helmet>
       <div className="custom-logo-wrapper">
         <img
